e8fed7b3a7ff391687c9cd4ca2769b6f4b934660
Eran <pavius@gmail.com>
2017-07-05 10:46:15 +0300

    minor appearence tweaks

--- vendor/github.com/nuclio/nuclio-zap/logger.go
+++ vendor/github.com/nuclio/nuclio-zap/logger.go
@@ -16,6 +16,7 @@ type NuclioZap struct {
 	coloredLevelInfo  string
 	coloredLevelWarn  string
 	coloredLevelError string
+	colorLoggerName func(string) string
 }
 
 func NewNuclioZap(name string) (*NuclioZap, error) {
@@ -55,7 +56,7 @@ func NewNuclioZap(name string) (*NuclioZap, error) {
 	newNuclioZap.SugaredLogger = newZapLogger.Sugar().Named(name)
 
 	// initialize coloring by level
-	newNuclioZap.initializeColoredLevels()
+	newNuclioZap.initializeColors()
 
 	return newNuclioZap, nil
 }
@@ -63,52 +64,52 @@ func NewNuclioZap(name string) (*NuclioZap, error) {
 func (nz *NuclioZap) Error(format interface{}, vars ...interface{}) {
 	formatString, formatIsString := format.(string)
 	if formatIsString {
-		nz.Errorf(formatString, vars...)
+		nz.SugaredLogger.Errorf(formatString, vars...)
 	} else {
-		nz.Error(format, vars...)
+		nz.SugaredLogger.Error(format)
 	}
 }
 
 func (nz *NuclioZap) ErrorWith(format interface{}, vars ...interface{}) {
-	nz.Errorw(format.(string), vars...)
+	nz.SugaredLogger.Errorw(format.(string), vars...)
 }
 
 func (nz *NuclioZap) Warn(format interface{}, vars ...interface{}) {
 	formatString, formatIsString := format.(string)
 	if formatIsString {
-		nz.Warnf(formatString, vars...)
+		nz.SugaredLogger.Warnf(formatString, vars...)
 	} else {
-		nz.Warn(format, vars...)
+		nz.SugaredLogger.Warn(format)
 	}
 }
 
 func (nz *NuclioZap) WarnWith(format interface{}, vars ...interface{}) {
-	nz.Warnw(format.(string), vars...)
+	nz.SugaredLogger.Warnw(format.(string), vars...)
 }
 
 func (nz *NuclioZap) Info(format interface{}, vars ...interface{}) {
 	formatString, formatIsString := format.(string)
 	if formatIsString {
-		nz.Infof(formatString, vars...)
+		nz.SugaredLogger.Infof(formatString, vars...)
 	} else {
-		nz.Info(format, vars...)
+		nz.SugaredLogger.Info(format)
 	}
 }
 
 func (nz *NuclioZap) InfoWith(format interface{}, vars ...interface{}) {
-	nz.Infow(format.(string), vars...)
+	nz.SugaredLogger.Infow(format.(string), vars...)
 }
 
 func (nz *NuclioZap) DebugWith(format interface{}, vars ...interface{}) {
-	nz.Debugw(format.(string), vars...)
+	nz.SugaredLogger.Debugw(format.(string), vars...)
 }
 
 func (nz *NuclioZap) Debug(format interface{}, vars ...interface{}) {
 	formatString, formatIsString := format.(string)
 	if formatIsString {
-		nz.Debugf(formatString, vars...)
+		nz.SugaredLogger.Debugf(formatString, vars...)
 	} else {
-		nz.Debug(format, vars...)
+		nz.SugaredLogger.Debug(format)
 	}
 }
 
@@ -117,24 +118,23 @@ func (nz *NuclioZap) Flush() {
 }
 
 func (nz *NuclioZap) GetChild(name string) interface{} {
-	return &NuclioZap{nz.Named(name), "", "", "", ""}
+	return &NuclioZap{SugaredLogger: nz.Named(name)}
 }
 
 func (nz *NuclioZap) encodeLoggerName(loggerName string, enc zapcore.PrimitiveArrayEncoder) {
 	const maxLoggerNameLength = 25
 	actualLoggerNameLength := len(loggerName)
+	var encodedLoggerName string
 
 	if actualLoggerNameLength >= maxLoggerNameLength {
-
-		// just truncate
-		enc.AppendString(loggerName[actualLoggerNameLength-maxLoggerNameLength:])
+		encodedLoggerName = loggerName[actualLoggerNameLength-maxLoggerNameLength:]
 
 	} else {
-
-		// left pad with spaces
-		enc.AppendString(strings.Repeat(" ", maxLoggerNameLength-actualLoggerNameLength-1))
-		enc.AppendString(loggerName)
+		encodedLoggerName = strings.Repeat(" ", maxLoggerNameLength-actualLoggerNameLength) + loggerName
 	}
+
+	// just truncate
+	enc.AppendString(nz.colorLoggerName(encodedLoggerName))
 }
 
 func (nz *NuclioZap) encodeStdoutLevel(level zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
@@ -157,9 +157,11 @@ func (nz *NuclioZap) encodeStdoutTime(t time.Time, enc zapcore.PrimitiveArrayEnc
 	enc.AppendString(t.Format("06.01.02 15:04:05.000"))
 }
 
-func (nz *NuclioZap) initializeColoredLevels() {
+func (nz *NuclioZap) initializeColors() {
 	nz.coloredLevelDebug = ansi.Color("(D)", "green")
 	nz.coloredLevelInfo = ansi.Color("(I)", "blue")
 	nz.coloredLevelWarn = ansi.Color("(W)", "yellow")
 	nz.coloredLevelError = ansi.Color("(E)", "red")
+
+	nz.colorLoggerName = ansi.ColorFunc("white")
 }
