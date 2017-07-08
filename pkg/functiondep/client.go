3eb5778ff3c1aef3cb64008213a202f9f6348284
Eran <pavius@gmail.com>
2017-07-08 23:08:28 +0300

    support HPA

--- pkg/functiondep/client.go
+++ pkg/functiondep/client.go
@@ -11,6 +11,7 @@ import (
 	"github.com/pkg/errors"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	autos_v1 "k8s.io/client-go/pkg/apis/autoscaling/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/pkg/api/v1"
 	v1beta1 "k8s.io/client-go/pkg/apis/apps/v1beta1"
@@ -91,6 +92,12 @@ func (c *Client) CreateOrUpdate(function *functioncr.Function) (*v1beta1.Deploym
 		return nil, errors.Wrap(err, "Failed to create/update deployment")
 	}
 
+	// create or update the HPA
+	_, err = c.createOrUpdateHorizontalPodAutoscaler(labels, function)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to create/update HPA")
+	}
+
 	c.logger.Debug("Deployment created/updated")
 
 	return deployment, nil
@@ -102,16 +109,18 @@ func (c *Client) Delete(namespace string, name string) error {
 		PropagationPolicy: &propogationPolicy,
 	}
 
-	// Delete Auto Scaler if exists
-	//err := c.clientSet.Autoscaling().HorizontalPodAutoscalers(namespace).Delete(name, delopt)
-	//if err != nil && !apierrors.IsNotFound(err) {
-	//	return err
-	//} else if !apierrors.IsNotFound(err){
-	//	common.LogDebug("Deleted HPA: %s %s",namespace, name)
-	//}
+	// Delete HPA if exists
+	err := c.clientSet.Autoscaling().HorizontalPodAutoscalers(namespace).Delete(name, deleteOptions)
+	if err != nil {
+		if !apierrors.IsNotFound(err) {
+			return errors.Wrap(err, "Failed to delete HPA")
+		}
+	} else {
+		c.logger.DebugWith("Deleted HPA", "namespace", namespace, "name", name)
+	}
 
 	// Delete Service if exists
-	err := c.clientSet.Core().Services(namespace).Delete(name, deleteOptions)
+	err = c.clientSet.Core().Services(namespace).Delete(name, deleteOptions)
 	if err != nil {
 		if !apierrors.IsNotFound(err) {
 			return errors.Wrap(err, "Failed to delete service")
@@ -248,6 +257,99 @@ func (c *Client) createOrUpdateDeployment(labels map[string]string,
 	c.logger.DebugWith("Service updated", "deployment", deployment)
 
 	return deployment, nil
+}
+
+func (c *Client) createOrUpdateHorizontalPodAutoscaler(labels map[string]string,
+	function *functioncr.Function) (*autos_v1.HorizontalPodAutoscaler, error) {
+
+	hpa, err := c.clientSet.Autoscaling().HorizontalPodAutoscalers(function.Namespace).Get(function.Name,
+		meta_v1.GetOptions{})
+
+	if err != nil {
+		if !apierrors.IsNotFound(err) {
+			return nil, errors.Wrap(err, "Failed to get HPA")
+		} else {
+
+			// signify that HPA doesn't exist
+			hpa = nil
+		}
+	}
+
+	// if an HPA exists and the replicas is non-zero
+	if hpa != nil && function.Spec.Replicas != 0 {
+		err = c.clientSet.Autoscaling().HorizontalPodAutoscalers(function.Namespace).Delete(function.Name,
+			&meta_v1.DeleteOptions{})
+
+		if err != nil {
+			return nil, errors.Wrap(err, "Failed to delete unnecessary HPA")
+		}
+
+		c.logger.Debug("HPA found, yet not needed. Deleted")
+
+		// HPA existed, but is no longer needed
+		return nil, nil
+	} else if hpa == nil && function.Spec.Replicas != 0 {
+		c.logger.Debug("HPA didn't exist and isn't needed")
+
+		// HPA didn't exist, and isn't needed
+		return nil, nil
+	}
+
+	maxReplicas := function.Spec.MaxReplicas
+	if maxReplicas == 0 {
+		maxReplicas = 4
+	}
+
+	minReplicas := function.Spec.MinReplicas
+	if minReplicas == 0 {
+		minReplicas = 1
+	}
+
+	targetCPU := int32(80)
+
+	// create new HPA
+	if hpa == nil {
+		hpa, err = c.clientSet.Autoscaling().HorizontalPodAutoscalers(function.Namespace).Create(&autos_v1.HorizontalPodAutoscaler{
+			ObjectMeta: meta_v1.ObjectMeta{
+				Name: function.Name,
+				Namespace: function.Namespace,
+				Labels: labels,
+			},
+			Spec: autos_v1.HorizontalPodAutoscalerSpec{
+				MinReplicas: &minReplicas,
+				MaxReplicas: maxReplicas,
+				TargetCPUUtilizationPercentage: &targetCPU,
+				ScaleTargetRef: autos_v1.CrossVersionObjectReference{
+					APIVersion: "extensions/v1beta1",
+					Kind: "Deployment",
+					Name: function.Name,
+				},
+
+			},
+		})
+
+		if err != nil {
+			return nil, errors.Wrap(err, "Failed to create HPA")
+		}
+
+		c.logger.DebugWith("Created HPA", "hpa", hpa)
+
+	} else {
+
+		hpa.Labels = labels
+		hpa.Spec.MinReplicas = &minReplicas
+		hpa.Spec.MaxReplicas = maxReplicas
+		hpa.Spec.TargetCPUUtilizationPercentage = &targetCPU
+		hpa, err = c.clientSet.Autoscaling().HorizontalPodAutoscalers(function.Namespace).Update(hpa)
+
+		if err != nil {
+			return nil, errors.Wrap(err, "Failed to update HPA")
+		}
+
+		c.logger.DebugWith("Updated HPA", "hpa", hpa)
+	}
+
+	return hpa, nil
 }
 
 func (c *Client) initClassLabels() {
