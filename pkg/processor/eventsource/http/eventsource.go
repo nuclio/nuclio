537671eecd2169860d67b1bd84107c511b0c0a64
Miki Tebeka <miki.tebeka@gmail.com>
2017-07-18 18:57:14 +0300

    do not catch panic in event handler (we will let k8s handle this)

--- pkg/processor/eventsource/http/eventsource.go
+++ pkg/processor/eventsource/http/eventsource.go
@@ -58,14 +58,6 @@ func (h *http) Stop(force bool) (eventsource.Checkpoint, error) {
 }
 
 func (h *http) requestHandler(ctx *fasthttp.RequestCtx) {
-	defer func() {
-		if err := recover(); err != nil {
-			h.Logger.ErrorWith("panic in request handlers", "err", err)
-			ctx.Response.SetStatusCode(net_http.StatusInternalServerError)
-			return
-		}
-	}()
-
 	// attach the context to the event
 	h.event.ctx = ctx
 
