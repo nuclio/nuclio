104567128fa8b4d78496974eaaa6261e272f9ea6
Miki Tebeka <miki.tebeka@gmail.com>
2017-07-24 13:07:01 +0300

    working e2e

--- test/e2e/run_e2e.go
+++ test/e2e/run_e2e.go
@@ -1,3 +1,12 @@
+/* End to end test
+
+We do the following steps:
+- Clearn k8s from previous deployment
+- Build and deploy a controller
+- Generate unique ID for this test
+- Build and deploy a HTTP function that returns the test ID in the response body
+- Perform a GET on the handler and see we got the right response
+*/
 package main
 
 import (
@@ -47,7 +56,7 @@ metadata:
   name: handler
 spec:
   replicas: 1
-  image: {{.Tag}}
+  image: localhost:5000/{{.Tag}}
   httpPort: {{.Port}}
 `))
 
@@ -184,25 +193,37 @@ func main() {
 		die("can't get nuclio - %s", err)
 	}
 
+	log.Printf("cleaning k8s from old deployment")
+	if err := runCmd("kubectl", "delete", "deploy,rs,ds,svc,po", "--all"); err != nil {
+		die("can't clear k8s cluster")
+	}
+
+	log.Printf("building controller")
 	srcDir := fmt.Sprintf("%s/src/github.com/nuclio/nuclio", gopath)
 	if err := os.Chdir(srcDir); err != nil {
 		die("can't change directory to %s", srcDir)
 	}
 
-	if err := runCmd("make", "controller"); err != nil {
+	if err := runCmd("make"); err != nil {
 		die("can't build controller - %s", err)
 	}
 
 	registry := fmt.Sprintf("%s:%d", options.k8sHost, options.registryPort)
 	tag := fmt.Sprintf("%s/%s", registry, imageName)
 	if err := runCmd("docker", "tag", imageName, tag); err != nil {
-		die("can't tag image - %s", err)
+		die("can't tag controller image - %s", err)
 	}
 
 	if err := runCmd("docker", "push", tag); err != nil {
-		die("can't push image - %s", err)
+		die("can't push controller image - %s", err)
 	}
 
+	ctrlFile := fmt.Sprintf("%s/hack/k8s/resources/controller.yaml", srcDir)
+	if err := runCmd("kubectl", "create", "-f", ctrlFile); err != nil {
+		die("can't deploy controller - %s", err)
+	}
+
+	log.Printf("building handler")
 	if err := runCmd("go", "get", "-d", "github.com/nuclio/nuclio-sdk"); err != nil {
 		die("can't get SDK - %s", err)
 	}
@@ -241,7 +262,7 @@ func main() {
 		die("can't build - %s", err)
 	}
 
-	params := kubeParame{Tag: tag, Port: options.port}
+	params := kubeParame{Tag: testID, Port: options.port}
 	cfgFile, err := ioutil.TempFile("", "e2e-config")
 	if err != nil {
 		die("can't create config file - %s", err)
@@ -254,15 +275,15 @@ func main() {
 		die("can't sync config file - %s", err)
 	}
 
-	// We don't care about error here, the create will fail as well
-	runCmd("kubectl", "delete", "--request-timeout", "1m", "-f", cfgFile.Name())
+	// Don't care about error here
+	runCmd("kubectl", "delete", "-f", cfgFile.Name())
 	if err := runCmd("kubectl", "create", "--request-timeout", "1m", "-f", cfgFile.Name()); err != nil {
 		die("can't create function - %s", err)
 	}
 
-	// TODO: How to check that handler is ready? Is it ready after previous kubectl command?
-	url := fmt.Sprintf("%s:%d", options.k8sHost, options.port)
-	log.Printf("getting %q", url)
+	time.Sleep(1 * time.Second) // Let controller get up
+	url := fmt.Sprintf("http://%s:%d", options.k8sHost, options.port)
+	log.Printf("calling handler at %q", url)
 	resp, err := http.Get(url)
 	if err != nil {
 		die("can't call handler - %s", err)
@@ -276,4 +297,5 @@ func main() {
 	if buf.String() != testID {
 		die("bad reply: got %q, expected %q", buf.String(), testID)
 	}
+	log.Printf("OK")
 }
