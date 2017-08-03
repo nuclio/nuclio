be2d049710658073b9dd09831faa179faeb4e607
Miki Tebeka <miki.tebeka@gmail.com>
2017-08-03 15:50:28 +0300

    Install kubectl locally

--- test/travis/install.sh
+++ test/travis/install.sh
@@ -5,7 +5,11 @@ set -e
 # Print commands
 set -x
 
+outfile=${HOME}/.local/bin/kubectl
+
 url_base=https://storage.googleapis.com/kubernetes-release/release
 kube_ver=$(curl -s ${url_base}/stable.txt)
-curl -L -o /usr/local/bin/kubectl ${url_base}/${kube_ver}/bin/linux/amd64/kubectl
-chmod +x /usr/local/bin/kubectl
+curl -L \
+    --create-dirs -o ${outfile} \
+    ${url_base}/${kube_ver}/bin/linux/amd64/kubectl
+chmod +x ${outfile}
