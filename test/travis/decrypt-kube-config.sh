ddb3b35182f889d17b760dcf0322df26c3752a61
Miki Tebeka <miki.tebeka@gmail.com>
2017-08-03 16:35:40 +0300

    Move file and skip delete

--- test/travis/decrypt-kube-config.sh
+++ test/travis/decrypt-kube-config.sh
@@ -3,18 +3,15 @@
 # ENCRYPTION_KEY is provided by travis
 
 cfg_file=~/.kube/config
+enc_file=./test/travis/kube_config.enc
 
 set -e
 
 if [ -f ${cfg_file} ]; then
     backup=${cfg_file}.$(date +%Y-%m-%d:%H:%M:%S)
-    cp -v ${cfg_file} ${backup}
+    mv -v ${cfg_file} ${backup}
 fi
 
 mkdir -p ~/.kube
-rm -f ${cfg_file}
-openssl aes-256-cbc \
-    -k "${ENCRYPTION_KEY}" \
-    -in ./test/travis/kube_config.enc \
-    -out ${cfg_file} \
-    -d
+echo "${enc_file} -> ${cfg_file}"
+openssl aes-256-cbc -k "${ENCRYPTION_KEY}" -in ${enc_file} -out ${cfg_file} \ -d
