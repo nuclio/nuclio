764f235d9e9fc89ebf39ad535e56ef926916e1b5
Miki Tebeka <miki.tebeka@gmail.com>
2017-08-03 15:39:38 +0300

    typo

--- test/travis/install.sh
+++ test/travis/install.sh
@@ -7,5 +7,5 @@ set -x
 
 url_base=https://storage.googleapis.com/kubernetes-release/release
 kube_ver=$(curl -s ${url_base}/stable.txt)
-curl -L -u /usr/local/bin/kubectl ${url_base}/${kube_ver}/bin/linux/amd64/kubectl
+curl -L -o /usr/local/bin/kubectl ${url_base}/${kube_ver}/bin/linux/amd64/kubectl
 chmod +x /usr/local/bin/kubectl
