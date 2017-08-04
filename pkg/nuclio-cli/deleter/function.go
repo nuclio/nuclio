cb3678fe58ed6ae8d08060619fee11db607e8e74
Eran <pavius@gmail.com>
2017-08-04 23:11:05 +0300

    added exec

--- pkg/nuclio-cli/deleter/function.go
+++ pkg/nuclio-cli/deleter/function.go
@@ -1,10 +1,6 @@
 package deleter
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
-
 	"github.com/nuclio/nuclio/pkg/functioncr"
 	"github.com/nuclio/nuclio/pkg/nuclio-cli"
 
@@ -45,55 +41,11 @@ func NewFunctionDeleter(parentLogger nuclio.Logger, options *Options) (*Function
 func (fd *FunctionDeleter) Execute() error {
 	var err error
 
-	resourceName, _, err := fd.parseResourceIdentifier(fd.options.ResourceIdentifier)
+	resourceName, _, err := nucliocli.ParseResourceIdentifier(fd.options.ResourceIdentifier)
 	if err != nil {
 		return errors.Wrap(err, "Failed to parse resource identifier")
 	}
 
 	// get specific function CR
 	return fd.functioncrClient.Delete(fd.options.Common.Namespace, resourceName, &meta_v1.DeleteOptions{})
-}
-
-func (fd *FunctionDeleter) parseResourceIdentifier(resourceIdentifier string) (resourceName string,
-	resourceVersion *string,
-	err error) {
-
-	// of the form: resourceName:resourceVersion or just resourceName
-	list := strings.Split(resourceIdentifier, ":")
-
-	// set the resource name
-	resourceName = list[0]
-
-	// only resource name provided
-	if len(list) == 1 {
-		return
-	}
-
-	// validate the resource version
-	if err = fd.validateVersion(list[1]); err != nil {
-		return
-	}
-
-	// set the resource version
-	resourceVersion = &list[1]
-
-	// if the resource is numeric
-	if *resourceVersion != "latest" {
-		resourceName = fmt.Sprintf("%s-%s", resourceName, *resourceVersion)
-	}
-
-	return
-}
-
-func (fd *FunctionDeleter) validateVersion(resourceVersion string) error {
-
-	// can be either "latest" or numeric
-	if resourceVersion != "latest" {
-		_, err := strconv.Atoi(resourceVersion)
-		if err != nil {
-			return errors.Wrap(err, `Version must be either "latest" or numeric`)
-		}
-	}
-
-	return nil
 }
