a7a4ca3743136e1fd761afba810e801b1b71926c
Eran <pavius@gmail.com>
2017-08-30 20:05:00 +0300

    using v3io-http which supports async

--- vendor/github.com/iguazio/v3io-go-http/context.go
+++ vendor/github.com/iguazio/v3io-go-http/context.go
@@ -1,48 +1,100 @@
 package v3io
 
 import (
-	"github.com/valyala/fasthttp"
+	"github.com/pkg/errors"
 )
 
 type Context struct {
-	logger     Logger
-	httpClient *fasthttp.HostClient
-	clusterURL string
+	logger      Logger
+	Sync        *SyncContext
+	requestChan chan *Request
+	numWorkers  int
 }
 
-func NewContext(parentLogger Logger, clusterURL string) (*Context, error) {
-	newClient := &Context{
-		logger: parentLogger.GetChild("v3io").(Logger),
-		httpClient: &fasthttp.HostClient{
-			Addr: clusterURL,
-		},
-		clusterURL: clusterURL,
+func NewContext(parentLogger Logger, clusterURL string, numWorkers int) (*Context, error) {
+	newSyncContext, err := newSyncContext(parentLogger, clusterURL)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to create sync context")
 	}
 
-	return newClient, nil
+	newContext := &Context{
+		logger:      parentLogger.GetChild("v3io").(Logger),
+		Sync:        newSyncContext,
+		requestChan: make(chan *Request, 1024),
+		numWorkers:  numWorkers,
+	}
+
+	for workerIndex := 0; workerIndex < numWorkers; workerIndex++ {
+		go newContext.workerEntry(workerIndex)
+	}
+
+	return newContext, nil
 }
 
 func (c *Context) NewSession(username string, password string, label string) (*Session, error) {
 	return newSession(c.logger, c, username, password, label)
 }
 
-func (c *Context) sendRequest(request *fasthttp.Request, response *fasthttp.Response) error {
-	c.logger.DebugWith("Sending request",
-		"method", string(request.Header.Method()),
-		"uri", string(request.Header.RequestURI()),
-		"body", string(request.Body()),
-	)
+func (c *Context) sendRequest(request *Request) error {
 
-	err := c.httpClient.Do(request, response)
-	if err != nil {
-		return err
-	}
-
-	// log the response
-	c.logger.DebugWith("Got response",
-		"statusCode", response.Header.StatusCode(),
-		"body", string(response.Body()),
-	)
+	// send the request to the request channel
+	c.requestChan <- request
 
 	return nil
+}
+
+func (c *Context) workerEntry(workerIndex int) {
+	for {
+		var response *Response
+		var err error
+
+		// read a request
+		request := <-c.requestChan
+
+		// according to the input type
+		switch typedInput := request.input.(type) {
+		case *ListAllInput:
+			response, err = request.container.Sync.ListAll()
+		case *ListBucketInput:
+			response, err = request.container.Sync.ListBucket(typedInput)
+		case *GetObjectInput:
+			response, err = request.container.Sync.GetObject(typedInput)
+		case *PutObjectInput:
+			err = request.container.Sync.PutObject(typedInput)
+		case *DeleteObjectInput:
+			err = request.container.Sync.DeleteObject(typedInput)
+		case *GetItemInput:
+			response, err = request.container.Sync.GetItem(typedInput)
+		case *GetItemsInput:
+			response, err = request.container.Sync.GetItems(typedInput)
+		case *PutItemInput:
+			err = request.container.Sync.PutItem(typedInput)
+		case *UpdateItemInput:
+			err = request.container.Sync.UpdateItem(typedInput)
+		default:
+			c.logger.ErrorWith("Got unexpected request type", "request", request)
+		}
+
+		// TODO: have the sync interfaces somehow use the pre-allocated response
+		if response != nil {
+			request.RequestResponse.Response = *response
+		}
+
+		response = &request.RequestResponse.Response
+
+		response.ID = request.ID
+		response.Error = err
+		response.RequestResponse = request.RequestResponse
+
+		// write to response channel
+		request.responseChan <- &request.RequestResponse.Response
+
+		// write the response to the channel (never block)
+		//switch {
+		//case request.responseChan <- response:
+		//	break
+		//default:
+		//	c.logger.WarnWith("Failed to write response to channel")
+		//}
+	}
 }
