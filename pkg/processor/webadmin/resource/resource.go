0bc760d544871dfeb114ad2d0bf3ca33c4ee63ff
Eran Duchan <pavius@gmail.com>
2017-09-09 21:58:53 +0300

    Added test for web admin resource (#165)

--- pkg/processor/webadmin/resource/resource.go
+++ pkg/processor/webadmin/resource/resource.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2017 The Nuclio Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package resource
 
 import (
@@ -31,7 +47,6 @@ type jsonapiResource struct {
 }
 
 type resource interface {
-	registerRoutes() error
 
 	// returns a list of custom routes for the resource
 	getCustomRoutes() map[string]customRoute
@@ -102,9 +117,15 @@ func (ar *abstractResource) registerRoutes() error {
 }
 
 func (ar *abstractResource) registerCustomRoutes() error {
+	customRouters := ar.resource.getCustomRoutes()
+
+	// not all resources support custom routes
+	if customRouters == nil {
+		return nil
+	}
 
 	// iterate through the custom routes and register a handler for them
-	for routePattern, customRoute := range ar.resource.getCustomRoutes() {
+	for routePattern, customRoute := range customRouters {
 		var routerFunc func(string, http.HandlerFunc)
 
 		switch customRoute.method {
@@ -118,8 +139,10 @@ func (ar *abstractResource) registerCustomRoutes() error {
 			routerFunc = ar.router.Delete
 		}
 
+		customRouteCopy := customRoute
+
 		routerFunc(routePattern, func(responseWriter http.ResponseWriter, request *http.Request) {
-			ar.callCustomRouteFunc(responseWriter, request, customRoute.routeFunc)
+			ar.callCustomRouteFunc(responseWriter, request, customRouteCopy.routeFunc)
 		})
 	}
 
@@ -208,6 +231,14 @@ func (ar *abstractResource) callCustomRouteFunc(responseWriter http.ResponseWrit
 
 	// see if the resource only supports a single record
 	resourceType, resources, single, _ := routeFunc(request)
+
+	if resources == nil {
+
+		// write a valid, empty JSON
+		responseWriter.Write([]byte("{}"))
+
+		return
+	}
 
 	// resource supports multiple instances
 	jsonapiResources := []jsonapiResource{}
