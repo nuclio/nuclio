7469c9502aa75107e94223ca797747e13c852c10
Eran <pavius@gmail.com>
2017-09-10 14:16:11 +0300

    revert demo

--- pkg/processor/runtime/golang/event_handler/demo.go
+++ pkg/processor/runtime/golang/event_handler/demo.go
@@ -21,10 +21,10 @@ import (
 )
 
 func Demo(context *nuclio.Context, event nuclio.Event) (interface{}, error) {
-	return nil, nuclio.ErrNotImplemented
+	return nil, nil
 }
 
 // uncomment to register demo
-func init() {
-	EventHandlers.Add("demo", Demo)
-}
+//func init() {
+//	EventHandlers.Add("demo", Demo)
+//}
