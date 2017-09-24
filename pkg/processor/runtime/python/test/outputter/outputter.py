baa241d0214dc094835a32d623f3c58bac8ee1f6
Miki Tebeka <miki.tebeka@gmail.com>
2017-09-24 16:47:31 +0300

    code format

--- pkg/processor/runtime/python/test/outputter/outputter.py
+++ pkg/processor/runtime/python/test/outputter/outputter.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 def handler(context, event):
     """Given a certain body, returns a response. Used by an integration test"""
 
