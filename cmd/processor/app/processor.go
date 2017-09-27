96e0b83e6cb126b7deb415e24a6c1eb7800a4889
Miki Tebeka <miki.tebeka@gmail.com>
2017-09-27 22:47:12 +0300

    Support NATS event source (#232)

--- cmd/processor/app/processor.go
+++ cmd/processor/app/processor.go
@@ -34,6 +34,7 @@ import (
 	_ "github.com/nuclio/nuclio/pkg/processor/eventsource/http"
 	_ "github.com/nuclio/nuclio/pkg/processor/eventsource/kafka"
 	_ "github.com/nuclio/nuclio/pkg/processor/eventsource/kinesis"
+	_ "github.com/nuclio/nuclio/pkg/processor/eventsource/nats"
 	_ "github.com/nuclio/nuclio/pkg/processor/eventsource/poller/v3ioitempoller"
 	_ "github.com/nuclio/nuclio/pkg/processor/eventsource/rabbitmq"
 	_ "github.com/nuclio/nuclio/pkg/processor/runtime/golang"
