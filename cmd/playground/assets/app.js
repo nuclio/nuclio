186881c30c253a604e4e468a728d125d636cf69a
Eran Nussbaum <eran-nussbaum@users.noreply.github.com>
2017-10-02 16:06:20 +0300

    Set {} as the initial value of Data Bindings input on view entry

--- cmd/playground/assets/app.js
+++ cmd/playground/assets/app.js
@@ -667,9 +667,9 @@ $(function () {
                 // otherwise - build HTML for list of key-value pairs, plus add headers
             } else {
                 pairList.append('<li>' + _(pairs).map(function (value, key) {
-                        return '<span class="pair-key">' + key + '</span>' +
-                            '<span class="pair-value">' + value + '</span>';
-                    }).join('</li><li>') + '</li>');
+                    return '<span class="pair-key">' + key + '</span>' +
+                           '<span class="pair-value">' + value + '</span>';
+                }).join('</li><li>') + '</li>');
 
                 var listItems = pairList.find('li'); // all list items
 
@@ -695,6 +695,7 @@ $(function () {
     };
 
     // init key-value pair inputs
+    dataBindingsEditor.setText('{}'); // initially data-bindings should be an empty object
     var labels = createKeyValuePairsInput('labels');
     var envVars = createKeyValuePairsInput('env-vars');
 
