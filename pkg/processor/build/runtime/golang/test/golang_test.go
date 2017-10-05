c2d83a3ebba9c63e11fb7db8af87dd8794715411
Eran <pavius@gmail.com>
2017-10-05 10:10:30 +0300

    add gofmt/staticcheck to linting

--- pkg/processor/build/runtime/golang/test/golang_test.go
+++ pkg/processor/build/runtime/golang/test/golang_test.go
@@ -18,6 +18,7 @@ package test
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"path"
 	"testing"
@@ -156,7 +157,7 @@ func (suite *TestSuite) TestBuildURL() {
 		path.Join(suite.getGolangDir(), "incrementor", "incrementor.go"),
 		"/some/path/incrementor.go")
 
-	defer httpServer.Shutdown(nil)
+	defer httpServer.Shutdown(context.TODO())
 
 	buildOptions := build.Options{
 		FunctionName: "incrementor",
