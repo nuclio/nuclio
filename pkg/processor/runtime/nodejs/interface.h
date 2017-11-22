a544708c8c1aa96a2d61db8cd7b8fb05e100ce04
Miki Tebeka <miki.tebeka@gmail.com>
2017-11-22 12:15:10 +0200

    calling a function

--- pkg/processor/runtime/nodejs/interface.h
+++ pkg/processor/runtime/nodejs/interface.h
@@ -1,5 +1,18 @@
-#ifndef JS_H
-#define JS_H
+/*
+Copyright 2017 The Nuclio Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 #ifdef __cplusplus
 extern "C" {
@@ -10,11 +23,18 @@ typedef struct {
   char *error_message;
 } new_result_t;
 
+typedef struct {
+  char *headers;
+  char *body;
+  char *content_type;
+  int status_code;
+  char *error_message;
+} response_t;
+
 void initialize();
 new_result_t new_worker(char *code, char *handler_name);
+response_t handle_event(void *worker, void *context, void *event);
 
 #ifdef __cplusplus
 }
-#endif
-
 #endif
--- pkg/processor/runtime/nodejs/runtime.go
+++ pkg/processor/runtime/nodejs/runtime.go
@@ -1,7 +1,23 @@
+/*
+Copyright 2017 The Nuclio Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
 import (
 	"fmt"
+	"unsafe"
 )
 
 /*
@@ -14,6 +30,7 @@ import "C"
 var (
 	jscode = `
 function handler(context, event) {
+	return 'OK';
 }
 `
 	handlerNamne = "handler"
@@ -22,10 +39,17 @@ function handler(context, event) {
 func main() {
 	C.initialize()
 	fmt.Println("Initialized")
-	worker := C.new_worker(C.CString(jscode), C.CString(handlerNamne))
-	fmt.Printf("WORKER: %+v\n", worker)
-	if worker.error_message != nil {
-		fmt.Printf("ERROR: %s\n", C.GoString(worker.error_message))
+	result := C.new_worker(C.CString(jscode), C.CString(handlerNamne))
+	fmt.Printf("WORKER: %+v\n", result)
+	if result.error_message != nil {
+		fmt.Printf("ERROR: %s\n", C.GoString(result.error_message))
+	}
+
+	resp := C.handle_event(result.worker, unsafe.Pointer(nil), unsafe.Pointer(nil))
+	if resp.error_message != nil {
+		fmt.Printf("ERROR: %s\n", C.GoString(resp.error_message))
+	} else {
+		fmt.Printf("BODY: %s\n", C.GoString(resp.body))
 	}
 	//C.set_handler(C.CString("add"), C.CString(jscode))
 }
