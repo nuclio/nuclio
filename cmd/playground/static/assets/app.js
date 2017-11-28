94c335bc665a3edf27404b25dfa83773dce9f3af
Eran Nussbaum <eran-nussbaum@users.noreply.github.com>
2017-11-28 17:23:43 +0200

    Spread top-bar's elements

--- cmd/playground/static/assets/app.js
+++ cmd/playground/static/assets/app.js
@@ -226,7 +226,7 @@ $(function () {
     // Tabs
     //
 
-    var initialTabIndex = 1;
+    var initialTabIndex = 0;
     var $tabContents = $('#tabs ~ section');
     var $tabHeaders = $('#tabs > ul > li');
     var $selectedTabHeader = $tabHeaders.eq(initialTabIndex);
