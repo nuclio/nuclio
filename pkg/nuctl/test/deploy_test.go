2850d8b6ced4c8ca8b424a3e04f190c817627d6f
Roy Lewin <roylewin@Roys-MacBook-Pro.local>
2017-12-10 16:23:42 +0200

    User doesn't need to pass path if handler is supplied when runtime is set to "shell"

--- pkg/nuctl/test/deploy_test.go
+++ pkg/nuctl/test/deploy_test.go
@@ -120,6 +120,20 @@ func (suite *DeployTestSuite) TestDeployWithMetadata() {
 	suite.Require().Contains(suite.outputBuffer.String(), "0099887766")
 }
 
+func (suite *DeployTestSuite) TestDeployFailsOnMissingPath() {
+	imageName := fmt.Sprintf("nuclio/deploy-test-%s", xid.New().String())
+
+	err := suite.ExecuteNutcl([]string{"deploy", "reverser", "--verbose", "--no-pull"},
+		map[string]string{
+			"nuclio-src-dir": suite.GetNuclioSourceDir(),
+			"image":          imageName,
+			"runtime":        "golang",
+			"handler":        "main:Reverse",
+		})
+
+	suite.Require().Error(err, "Function code must be provided either in the path or inline in a spec file; alternatively, an image or handler may be provided")
+}
+
 func TestDeployTestSuite(t *testing.T) {
 	if testing.Short() {
 		return
