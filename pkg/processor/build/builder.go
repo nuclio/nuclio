80ad752223625342fdf58575b6b3676418f7a9c4
Roy Lewin <roylewin@Roys-MacBook-Pro.local>
2017-12-11 15:35:20 +0200

    WIP - deploy function accepts not receiving "runtime" if given a directory/archive that includes function.yaml

--- pkg/processor/build/builder.go
+++ pkg/processor/build/builder.go
@@ -420,7 +420,7 @@ func (b *Builder) getRuntimeName() (string, error) {
 	if runtimeName == "" {
 
 		// if the function path is a directory, runtime must be specified in the command-line arguments or configuration
-		if common.IsDir(b.options.FunctionConfig.Spec.Build.Path) {
+		if common.IsDir(b.options.FunctionConfig.Spec.Build.Path) && !common.FileExists(path.Join(b.options.FunctionConfig.Spec.Build.Path, "function.yaml")) {
 			return "", errors.New("Build path is directory - runtime must be specified")
 		}
 
