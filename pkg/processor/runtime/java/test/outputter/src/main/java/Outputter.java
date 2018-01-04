2f6cd8a93e98e7294c0ec60dfdeaeb41b4818b39
Miki Tebeka <miki.tebeka@gmail.com>
2018-01-04 15:39:33 +0200

    license and .equals

--- pkg/processor/runtime/java/test/outputter/src/main/java/Outputter.java
+++ pkg/processor/runtime/java/test/outputter/src/main/java/Outputter.java
@@ -1,47 +1,64 @@
+/*
+Copyright 2017 The Nuclio Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+// IF YOU MAKE CHANGES TO THIS FILE RUN "gradle jar" *BEFORE* RUNNING TESTS
+// TODO: Find a way to automate this in the tests
+
 import io.nuclio.Context;
 import io.nuclio.Event;
 import io.nuclio.EventHandler;
 import io.nuclio.Response;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 public class Outputter implements EventHandler {
     @Override
     public Response handleEvent(Context context, Event event) {
-        if (event.getMethod() != "POST") {
+        if (!event.getMethod().equals("POST")) {
             return new Response().setBody(event.getMethod());
         }
 
         String body = new String(event.getBody());
 
-        if (body == "return_string") {
+        if (body.equals("return_string")) {
             return new Response().setBody("a string");
-        } else if (body == "return_bytes") {
+        } else if (body.equals("return_bytes")) {
             return new Response().setBody("bytes".getBytes());
-        } else if (body == "log") {
+        } else if (body.equals("log")) {
             context.getLogger().debug("Debug message");
             context.getLogger().info("Info message");
             context.getLogger().warn("Warn message");
             context.getLogger().error("Error message");
 
             return new Response().setBody("returned logs").setStatusCode(201);
-        } else if (body == "log_with") {
+        } else if (body.equals("log_with")) {
             context.getLogger().errorWith(
                     "Error message", "source", "rabbit", "weight", 7);
             return new Response().setBody("returned logs with").setStatusCode(201);
-        } else if (body == "return response") {
-            Map<String, Object> headers = Collections.emptyMap();
+        } else if (body.equals("return_response")) {
+            Map<String, Object> headers = new HashMap<String, Object>();
             headers.put("h1", "v1");
-            headers.put("h1", "v2");
+            headers.put("h2", "v2");
 
             return new Response().setBody("response body").setHeaders(headers)
                     .setContentType("text/plain").setStatusCode(201);
-        } else if (body == "return fields") {
-            List<String> fields = Collections.emptyList();
+        } else if (body.equals("return_fields")) {
+            List<String> fields = new ArrayList<String>();
             for (Map.Entry<String, Object> entry : event.getFields().entrySet()) {
                 fields.add(String.format("%s=%s", entry.getKey(), entry.getValue()));
             }
@@ -49,9 +66,9 @@ public class Outputter implements EventHandler {
 
             String outBody = String.join(",", fields);
             return new Response().setBody(outBody);
-        } else if (body == "return_path") {
+        } else if (body.equals("return_path")) {
             return new Response().setBody(event.getPath());
-        } else if (body == "return_error") {
+        } else if (body.equals("return_error")) {
             throw new RuntimeException("some error");
         }
 
