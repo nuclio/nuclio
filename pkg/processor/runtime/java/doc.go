f41bc03125db25f65eb1b494b59ebc63f3ef5ba7
Miki Tebeka <miki.tebeka@gmail.com>
2018-01-07 16:18:51 +0200

    Document JDK version

--- pkg/processor/runtime/java/doc.go
+++ pkg/processor/runtime/java/doc.go
@@ -3,6 +3,8 @@
 To implement a handler you need to write a class the implements
 io.nuclio.EventHandler and then build a jar to pass nuclio builder.
 
+The default image is using JDK 8
+
 If you have dependecies in other packages, create a fat/uber Jar with all the
 dependencies. We currently do not support maven/gradle/sbt/ant/... builds
 */
