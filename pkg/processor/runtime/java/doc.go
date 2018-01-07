4ddd45aab04af7e6adb393d90757859b7b2edba8
Miki Tebeka <miki.tebeka@gmail.com>
2018-01-07 09:46:09 +0200

    doc

---
+++ pkg/processor/runtime/java/doc.go
@@ -0,0 +1,9 @@
+/* package java implements a Java runtime
+
+To implement a handler you need to write a class the implements
+io.nuclio.EventHandler and then build a jar to pass nuclio builder.
+
+If you have dependecies in other packages, create a fat/uber Jar with all the
+dependencies. We currently do not support maven/gradle/sbt/ant/... builds
+*/
+package java
