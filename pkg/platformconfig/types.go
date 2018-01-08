65f112c199db569444c684a5c450fc53951906c3
Eran Duchan <pavius@gmail.com>
2018-01-08 20:38:26 +0200

    Functions receive platform config - respect only metrics (#493)
    1. On deploy, the processor attempts to open /etc/nuclio/configs/platform/platform.yaml`. This has the following format:
    
    ```
    webAdmin:
      enabled: true
      listenAddress: :8081
    logger:
      sinks:
        stdout:
          driver: stdout
        staging-es:
          driver: elasticsearch
          url: http://10.0.0.1:9200
        prod-es:
          driver: elasticsearch
          url: http://20.0.1:9200
          attributes:
            dontCrash: true
      system:
      - level: debug
        sink: prod-es
      - level: info
        sink: stdout
      functions:
      - level: info
        sink: stdout
    metrics:
      sinks:
        mypush:
          driver: prometheusPush
          url: 10.0.0.1:30
          attributes:
            interval: 10s
      system:
      - mypush
      functions:
      - mypush
    ```
    
    If the file doesn't exist, sane defaults are loaded (logs to stdout, metrics and webadmin disabled).
    
    At the moment, only `metrics` is respected by the processor though the `platformconfig` module handles all aspects of this configuration.
    
    2. Controller, on deployment, will attempt to map the `platform-config` configmap in the function's namespace to `/etc/nuclio/configs/platform/platform.yaml`. If this configmap exists, it should contain a valid platform configuration
    
    3. Controller can be configured to listen for function changes on specific namespaces, as per the `NUCLIO_CONTROLLER_NAMESPACE` environment variable:
    3.1. Lack of this env variable or `*` means listen on all namespaces
    3.2. A value of `@nuclio.selfNamespace` means the controller will only listen for functions created on its namespace
    3.3. Any other value (e.g. "default", "foo") will listen for function changes on that namespace

---
+++ pkg/platformconfig/types.go
@@ -0,0 +1,61 @@
+/*
+Copyright 2017 The Nuclio Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package platformconfig
+
+type LoggerSink struct {
+	Driver     string                 `json:"driver,omitempty"`
+	URL        string                 `json:"url,omitempty"`
+	Attributes map[string]interface{} `json:"attributes,omitempty"`
+}
+
+type LoggerSinkWithLevel struct {
+	Level string
+	Sink  LoggerSink
+}
+
+type LoggerSinkBinding struct {
+	Level string `json:"level,omitempty"`
+	Sink  string `json:"sink,omitempty"`
+}
+
+type FunctionsLogger struct {
+	DefaultLevel string `json:"defaultLevel,omitempty"`
+	DefaultSink  string `json:"defaultSink,omitempty"`
+}
+
+type Logger struct {
+	Sinks     map[string]LoggerSink `json:"sinks,omitempty"`
+	System    []LoggerSinkBinding   `json:"system,omitempty"`
+	Functions []LoggerSinkBinding   `json:"functions,omitempty"`
+}
+
+type WebAdmin struct {
+	Enabled       bool   `json:"enabled,omitempty"`
+	ListenAddress string `json:"listenAddress,omitempty"`
+}
+
+type MetricSink struct {
+	Driver     string                 `json:"driver,omitempty"`
+	URL        string                 `json:"url,omitempty"`
+	Attributes map[string]interface{} `json:"attributes,omitempty"`
+}
+
+type Metrics struct {
+	Sinks     map[string]MetricSink `json:"sinks,omitempty"`
+	System    []string              `json:"system,omitempty"`
+	Functions []string              `json:"functions,omitempty"`
+}
