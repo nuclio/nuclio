5dceabd01c83424201376f33cff86737ff73c6ef
Roy Lewin <balster1123@gmail.com>
2018-01-14 19:07:59 +0200

    Cron Trigger (#487)
    Fixes #317

---
+++ pkg/processor/trigger/cron/types.go
@@ -0,0 +1,30 @@
+package cron
+
+import (
+	"github.com/nuclio/nuclio/pkg/errors"
+	"github.com/nuclio/nuclio/pkg/functionconfig"
+	"github.com/nuclio/nuclio/pkg/processor/trigger"
+
+	"github.com/mitchellh/mapstructure"
+)
+
+type Configuration struct {
+	trigger.Configuration
+	Schedule string
+	Interval string
+	Event    Event
+}
+
+func NewConfiguration(ID string, triggerConfiguration *functionconfig.Trigger) (*Configuration, error) {
+	newConfiguration := Configuration{}
+
+	// create base
+	newConfiguration.Configuration = *trigger.NewConfiguration(ID, triggerConfiguration)
+
+	// parse attributes
+	if err := mapstructure.Decode(newConfiguration.Configuration.Attributes, &newConfiguration); err != nil {
+		return nil, errors.Wrap(err, "Failed to decode attributes")
+	}
+
+	return &newConfiguration, nil
+}
