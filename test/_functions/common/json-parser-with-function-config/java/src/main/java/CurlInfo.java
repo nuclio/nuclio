e18dad2554e3535524f264a8e304069c2774aac2
Miki Tebeka <miki.tebeka@gmail.com>
2018-01-17 20:58:42 -0800

    handler works

--- test/_functions/common/json-parser-with-function-config/java/src/main/java/CurlInfo.java
+++ test/_functions/common/json-parser-with-function-config/java/src/main/java/CurlInfo.java
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.core.type.TypeReference;
 import io.nuclio.Context;
 import io.nuclio.Event;
 import io.nuclio.EventHandler;
@@ -5,34 +6,34 @@ import io.nuclio.Response;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-class Request {
-	@JsonProperty("return_this")
-	String returnThis;
-}
-
 
 public class CurlInfo implements EventHandler {
-	@Override
-	public Response handleEvent(Context context, Event event) {
-		try {
-			if (!curlInstalled()) {
-				return new Response().setBody("curl not installed").setStatusCode(500);
-			}
+    @Override
+    public Response handleEvent(Context context, Event event) {
+        try {
+            if (!curlInstalled()) {
+                return new Response().setBody("curl not installed").setStatusCode(500);
+            }
 
-			ObjectMapper mapper = new ObjectMapper();
-			Request request = mapper.readValue(event.getBody(), Request.class);
-			return new Response().setBody(request.returnThis);
-		} catch (Throwable err) {
-			return new Response().setBody(err.toString()).setStatusCode(500);
-		}
-	}
+            ObjectMapper mapper = new ObjectMapper();
+            TypeReference<HashMap<String, String>> typeRef = new TypeReference<HashMap<String, String>>() {
+            };
 
-	private boolean curlInstalled() throws Throwable {
-       Process process = new ProcessBuilder("curl", "--version").start();
-		return process.exitValue() == 0;
-	}
+            Map<String, String> request = mapper.readValue(event.getBody(), typeRef);
+            return new Response().setBody(request.get("return_this"));
+        } catch (Throwable err) {
+            return new Response().setBody(err.toString()).setStatusCode(500);
+        }
+    }
+
+    private boolean curlInstalled() throws Throwable {
+        Process process = new ProcessBuilder("curl", "--version").start();
+        return process.waitFor() == 0;
+    }
 }
