fc846fe6dab0f0cefaef6d2c6465a164f96cda9b
Miki Tebeka <miki.tebeka@gmail.com>
2018-01-19 18:39:08 -0800

    Switch to openjdk docker since java is deprecated

--- pkg/processor/runtime/java/doc.go
+++ pkg/processor/runtime/java/doc.go
@@ -4,7 +4,7 @@ Package java implements a Java runtime
 To implement a handler you need to write a class the implements
 io.nuclio.EventHandler and then build a jar to pass nuclio builder.
 
-The default image is using JDK 8
+The default image is using OpenJDK 9
 
 If you have dependecies in other packages, create a fat/uber Jar with all the
 dependencies. We currently do not support maven/gradle/sbt/ant/... builds
