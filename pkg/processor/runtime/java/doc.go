9ab90793df0a5632cc6df37d3f554e0b780df468
Miki Tebeka <miki.tebeka@gmail.com>
2018-01-28 19:58:36 +0200

    document how to add dependencies

--- pkg/processor/runtime/java/doc.go
+++ pkg/processor/runtime/java/doc.go
@@ -1,8 +1,25 @@
 /*
 Package java implements a Java runtime
-
+u c
 To implement a handler you need to write a class the implements
 io.nuclio.EventHandler and then build a jar to pass nuclio builder.
+
+You can also pass a Java file to `nuctl build` and it'll generate a jar from it.
+You can specify dependencies using (inline) build configuration
+
+// @nuclio.configure
+//
+// function.yaml:
+//   spec:
+//     build:
+//       dependencies:
+//         - group: com.google.code.gson"
+//           name: gson"
+//           version: 2.8.2"
+//         - group: com.google.guava"
+//           name: guava"
+//           version: 23.6-jre"
+
 
 The default image is using OpenJDK 9
 
