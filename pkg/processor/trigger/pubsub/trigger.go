735ed2d5d14d79baddbcb346fb48d71dd692657e
Tony Rewin <tony@lampheads.ru>
2018-02-05 04:35:17 +0300

    nack fixes, single option added

--- pkg/processor/trigger/pubsub/trigger.go
+++ pkg/processor/trigger/pubsub/trigger.go
@@ -17,101 +17,103 @@ limitations under the License.
 package pubsub
 
 import (
-	"fmt"
-	"time"
+    "fmt"
+    "time"
 
-	"github.com/nuclio/nuclio/pkg/common"
-	"github.com/nuclio/nuclio/pkg/errors"
-	"github.com/nuclio/nuclio/pkg/processor/trigger"
-	"github.com/nuclio/nuclio/pkg/processor/worker"
+    "github.com/nuclio/nuclio/pkg/common"
+    "github.com/nuclio/nuclio/pkg/errors"
+    "github.com/nuclio/nuclio/pkg/processor/trigger"
+    "github.com/nuclio/nuclio/pkg/processor/worker"
 
-	ps "cloud.google.com/go/pubsub"
-	"github.com/nuclio/logger"
-	"github.com/rs/xid"
+    ps "cloud.google.com/go/pubsub"
+    "github.com/nuclio/logger"
+    "github.com/rs/xid"
 )
 
 type pubsub struct {
-	trigger.AbstractTrigger
-	event              Event
-	configuration      *Configuration
-	stop               chan bool
-	pubsubSubscription *ps.Subscription
+    trigger.AbstractTrigger
+    event              Event
+    configuration      *Configuration
+    stop               chan bool
+    pubsubSubscription *ps.Subscription
 }
 
 func newTrigger(parentLogger logger.Logger,
-	workerAllocator worker.Allocator,
-	configuration *Configuration) (trigger.Trigger, error) {
+    workerAllocator worker.Allocator,
+    configuration *Configuration) (trigger.Trigger, error) {
 
-	newTrigger := &pubsub{
-		AbstractTrigger: trigger.AbstractTrigger{
-			ID:              configuration.ID,
-			Logger:          parentLogger.GetChild(configuration.ID),
-			WorkerAllocator: workerAllocator,
-			Class:           "async",
-			Kind:            "pubsub",
-		},
-		configuration: configuration,
-		stop:          make(chan bool),
-	}
+    newTrigger := &pubsub{
+        AbstractTrigger: trigger.AbstractTrigger{
+            ID:              configuration.ID,
+            Logger:          parentLogger.GetChild(configuration.ID),
+            WorkerAllocator: workerAllocator,
+            Class:           "async",
+            Kind:            "pubsub",
+        },
+        configuration: configuration,
+        stop:          make(chan bool),
+    }
 
-	return newTrigger, nil
+    return newTrigger, nil
 }
 
 func (n *pubsub) Start(checkpoint trigger.Checkpoint) error {
-	n.Logger.InfoWith("Starting",
-		"topic", n.configuration.Topic,
-		"project", n.configuration.Project,
-	)
-	ctx := context.Background()
+    n.Logger.InfoWith("Starting",
+        "topic", n.configuration.Topic,
+        "project", n.configuration.Project,
+    )
+    ctx := context.Background()
 
-	// pubsub client consumes namespace/project string to be created
-	client, err := ps.NewClient(ctx, n.configuration.Project)
-	if err != nil {
-		return errors.Wrapf(err, "Can't connect to pubsub project")
-	}
+    // pubsub client consumes namespace/project string to be created
+    client, err := ps.NewClient(ctx, n.configuration.Project)
+    if err != nil {
+        return errors.Wrapf(err, "Can't connect to pubsub project")
+    }
 
-	// every replica will get a unique id
-	subName := fmt.Sprintf("nuclio-trigger-%s", xid.New().String())
-	sub, err := client.CreateSubscription(ctx, subName, ps.SubscriptionConfig{
-		Topic:       n.configuration.Topic,
-		AckDeadline: 10 * time.Second,
-	})
+    // every replica will get a unique id
+    subName := fmt.Sprintf("nuclio-trigger-%s", xid.New().String())
+    sub := client.CreateSubscription(ctx, subName, ps.SubscriptionConfig{
+        Topic:       n.configuration.Topic,
+        AckDeadline: 10 * time.Second,
+    })
 
-	// https://godoc.org/cloud.google.com/go/pubsub#ReceiveSettings
-	sub.ReceiveSettings.NumGoroutines = n.configuration.MaxWorkers
+    // https://godoc.org/cloud.google.com/go/pubsub#ReceiveSettings
+    sub.ReceiveSettings.NumGoroutines = n.configuration.MaxWorkers
 
-	// listen to subscribed topic messages
-	err = sub.Receive(ctx, func(ctx context.Context, m *ps.Message) {
-		// NOTE: May be called concurrently; synchronize access to shared memory.
-		n.event.psMessage = m
+    // listen to subscribed topic messages
+    err = sub.Receive(ctx, func(ctx context.Context, m *ps.Message) {
+        // NOTE: May be called concurrently; synchronize access to shared memory.
+        n.event.psMessage = m
 
-		// process the event, don't really do anything with response
-		_, submitError, processError := n.AllocateWorkerAndSubmitEvent(&n.event, n.Logger, 10*time.Second)
-		if submitError != nil {
-			n.Logger.ErrorWith("Can't submit event", "error", submitError)
-			m.Nack() // necessary to call on fail
-		}
-		if processError != nil {
-			n.Logger.ErrorWith("Can't process event", "error", processError)
-			m.Nack()
-		}
-		m.Ack()
-	})
-	if err != context.Canceled {
-		return errors.Wrapf(err, "Message receiver cancelled")
-	}
-	return nil
+        // process the event, don't really do anything with response
+        _, submitError, processError := n.AllocateWorkerAndSubmitEvent(&n.event, n.Logger, 10*time.Second)
+        if submitError != nil {
+            n.Logger.ErrorWith("Can't submit event", "error", submitError)
+            m.Nack() // necessary to call on fail
+            return
+        }
+        if processError != nil {
+            n.Logger.ErrorWith("Can't process event", "error", processError)
+            m.Nack()
+            return
+        }
+        m.Ack()
+    })
+    if err != context.Canceled {
+        return errors.Wrapf(err, "Message receiver cancelled")
+    }
+    return nil
 }
 
 func (n *pubsub) Stop(force bool) (trigger.Checkpoint, error) {
-	n.stop <- true
-	ctx := context.Background()
-	if err := n.pubsubSubscription.Delete(ctx); err != nil {
-		return errors.Wrapf(err, "Delete subscription")
-	}
-	return nil
+    n.stop <- true
+    ctx := context.Background()
+    if err := n.pubsubSubscription.Delete(ctx); err != nil {
+        return errors.Wrapf(err, "Delete subscription")
+    }
+    return nil
 }
 
 func (n *pubsub) GetConfig() map[string]interface{} {
-	return common.StructureToMap(n.configuration)
+    return common.StructureToMap(n.configuration)
 }
