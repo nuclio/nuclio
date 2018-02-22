81ac36f79dfb698a2b5970ec34e7e4fdfc6ab914
texnichnyi <yuriikushpit@gmail.com>
2018-02-22 15:23:43 +0200

    Fix all mentioned issues in PR Add few unit test for helper.go functionality #604

--- pkg/common/helper_test.go
+++ pkg/common/helper_test.go
@@ -3,6 +3,7 @@ package common
 import (
 	"io/ioutil"
 	"os"
+	"runtime"
 	"testing"
 	"time"
 
@@ -14,9 +15,75 @@ type IsFileTestSuite struct {
 	tempDir string
 }
 
+func (suite *IsFileTestSuite) TestPositive() {
+	// Create temp file
+	tempFile, err := ioutil.TempFile(suite.tempDir, "temp_file")
+	suite.Require().NoError(err)
+	defer os.Remove(tempFile.Name())
+
+	// Verify that function isFile() returns true when file is created
+	suite.Require().True(IsFile(tempFile.Name()))
+
+}
+
+func (suite *IsFileTestSuite) TestFileIsNotExist() {
+	// Create temp file
+	tempFile, err := ioutil.TempFile(suite.tempDir, "temp_file")
+	suite.Require().NoError(err)
+	os.Remove(tempFile.Name())
+
+	// Verify that function isFile() returns false when file doesn't exist in the system
+	suite.Require().False(IsFile(tempFile.Name()))
+}
+
+func (suite *IsFileTestSuite) TestFileIsADirectory() {
+	var err error
+
+	// Create temp directory
+	suite.tempDir, err = ioutil.TempDir("", "isfile-test")
+	suite.Require().NoError(err)
+	defer os.RemoveAll(suite.tempDir)
+
+	suite.Require().False(IsFile(suite.tempDir))
+
+	// Set up temp dir in empty string to prevent no such file or directory
+	// When next functions will creates file from /tmp/path_to_dir/path_to_file_which_func_want_to_create
+	suite.tempDir = ""
+}
+
+func TestIsFileTestSuite(t *testing.T) {
+	suite.Run(t, new(IsFileTestSuite))
+}
+
 type IsDirTestSuite struct {
 	suite.Suite
 	tempDir string
+}
+
+func (suite *IsDirTestSuite) TestPositive() {
+	var err error
+
+	// Create a temp directory
+	suite.tempDir, err = ioutil.TempDir("", "isdir-test")
+	suite.Require().NoError(err)
+	defer os.RemoveAll(suite.tempDir)
+
+	// Verify that function IsDir() returns true when directory exists in the system
+	suite.Require().True(IsDir(suite.tempDir))
+}
+
+func (suite *IsDirTestSuite) TestNegative() {
+	// Create temp file
+	tempFile, err := ioutil.TempFile(suite.tempDir, "temp_file")
+	suite.Require().NoError(err)
+	defer os.Remove(tempFile.Name())
+
+	// Verify that function IsDir( returns false when file instead of directory is function argument
+	suite.Require().False(IsDir(tempFile.Name()))
+}
+
+func TestIsDirTestSuite(t *testing.T) {
+	suite.Run(t, new(IsDirTestSuite))
 }
 
 type FileExistTestSuite struct {
@@ -24,142 +91,140 @@ type FileExistTestSuite struct {
 	tempDir string
 }
 
-type StringSliceToIntSliceTestSuite struct {
-	suite.Suite
-}
-
-type RetryUntilSuccessfulTestSuite struct {
-	suite.Suite
-}
-
-func (suite *IsFileTestSuite) SetupTest()  {
-	var err error
-
-	// create a temp directory
-	suite.tempDir, err = ioutil.TempDir("", "isfile-test")
-	suite.Require().NoError(err)
-}
-
-func (suite *IsFileTestSuite) TestIsFileFuncPositive() {
-	//create temp file
+func (suite *FileExistTestSuite) TestPositive() {
+	// Create temp file
 	tempFile, err := ioutil.TempFile(suite.tempDir, "temp_file")
 	suite.Require().NoError(err)
-	suite.Assert().True(IsFile(tempFile.Name()))
 	defer os.Remove(tempFile.Name())
-}
 
-func (suite *IsFileTestSuite) TestIsFileFuncFileIsNotExist() {
-	//create temp file
-	tempFile, err := ioutil.TempFile(suite.tempDir, "temp_file")
-	suite.Require().NoError(err)
-	os.Remove(tempFile.Name())
-	suite.Assert().False(IsFile(tempFile.Name()))
-}
-
-func (suite *IsFileTestSuite) TestIsFileFuncFileIsADirectory() {
-	suite.Assert().False(IsFile(suite.tempDir))
-}
-
-func (suite *IsFileTestSuite) TearDown() {
-	defer os.RemoveAll(suite.tempDir)
-}
-
-func (suite *IsDirTestSuite) TestIsDirFuncPositive() {
-	var err error
-
-	// create a temp directory
-	suite.tempDir, err = ioutil.TempDir("", "isdir-test")
-	suite.Require().NoError(err)
-	suite.Assert().True(IsDir(suite.tempDir))
-}
-
-func (suite *IsDirTestSuite) TestIsDirFuncNegative() {
-	//create temp file
-	tempFile, err := ioutil.TempFile(suite.tempDir, "temp_file")
-	suite.Require().NoError(err)
-	suite.Assert().False(IsDir(tempFile.Name()))
-	defer os.Remove(tempFile.Name())
-}
-
-func (suite *IsDirTestSuite) TearDown() {
-	defer os.RemoveAll(suite.tempDir)
-}
-
-func (suite *FileExistTestSuite) SetupTest()  {
-	var err error
-
-	// create a temp directory
-	suite.tempDir, err = ioutil.TempDir("", "file_exist-test")
-	suite.Require().NoError(err)
-}
-
-func (suite *FileExistTestSuite) TestFileExistsFuncPositive() {
-	//create temp file
-	tempFile, err := ioutil.TempFile(suite.tempDir, "temp_file")
-	suite.Require().NoError(err)
-	suite.Assert().True(IsFile(tempFile.Name()))
-	defer os.Remove(tempFile.Name())
+	// Verify that function FileExists() returns true when file is exist
+	suite.Require().True(FileExists(tempFile.Name()))
 }
 
 func (suite *FileExistTestSuite) TestFileNotExist() {
-	//create temp file
+	// Create temp file
 	tempFile, err := ioutil.TempFile(suite.tempDir, "temp_file")
 	suite.Require().NoError(err)
 	os.Remove(tempFile.Name())
-	suite.Assert().False(IsFile(tempFile.Name()))
+
+	// Verify that function FileExists() returns false when file doesn't exist
+	suite.Require().False(FileExists(tempFile.Name()))
 }
 
-func (suite *FileExistTestSuite) TestFileExistFuncFileIsNotAFile() {
-	suite.Assert().False(IsFile(suite.tempDir))
-}
+func (suite *FileExistTestSuite) TestFileIsNotAFile() {
+	var err error
 
-func (suite *FileExistTestSuite) TearDown() {
+	// Create temp directory
+	suite.tempDir, err = ioutil.TempDir("", "file_exists-test")
+	suite.Require().NoError(err)
 	defer os.RemoveAll(suite.tempDir)
-}
 
-func (suite *StringSliceToIntSliceTestSuite) TestStringSliceToIntSlicePositive() {
-	stringSlice := []string{"1","2","5","6","23"}
-	expectedSlice := []int{1,2,5,6,23}
-	actualSlice, err := StringSliceToIntSlice(stringSlice);
-	suite.Assert().Equal(expectedSlice,actualSlice)
-	suite.Assert().Equal(nil,err)
-}
+	// Verify that function returns
+	suite.Require().True(FileExists(suite.tempDir))
 
-func (suite *StringSliceToIntSliceTestSuite) TestStringSliceToIntSliceNegativeData() {
-	stringSlice := []string{"1","2","5","6","23", "someBadData"}
-	_, err := StringSliceToIntSlice(stringSlice);
-	suite.Assert().True(err!=nil)
-}
-
-func (suite *RetryUntilSuccessfulTestSuite) TestRetryUntilSuccessfulPositive() {
-	err:= RetryUntilSuccessful(2*time.Second, 1*time.Second, func() bool {
-		return true
-	})
-	suite.Assert().True(err==nil)
-}
-
-func (suite *RetryUntilSuccessfulTestSuite) TestRetryUntilSuccessfulNegative() {
-	err:= RetryUntilSuccessful(2*time.Second, 1*time.Second, func() bool {
-		return false
-	})
-	suite.Assert().True(err!=nil)
-
-}
-
-func TestIsFileTestSuite(t *testing.T) {
-	suite.Run(t, new(IsFileTestSuite))
-}
-
-func TestIsDirTestSuite(t *testing.T) {
-	suite.Run(t, new(IsDirTestSuite))
+	// Set up temp dir in empty string to prevent no such file or directory
+	// When next functions will creates file from /tmp/path_to_dir/path_to_file_which_func_want_to_create
+	suite.tempDir = ""
 }
 
 func TestFileExistsTestSuite(t *testing.T) {
 	suite.Run(t, new(FileExistTestSuite))
 }
 
+type StringSliceToIntSliceTestSuite struct {
+	suite.Suite
+}
+
+func (suite *StringSliceToIntSliceTestSuite) TestPositive() {
+	// Prepare slice for StringSliceToIntSlice() function
+	stringSlice := []string{"1", "2", "5", "6", "23"}
+	expectedSlice := []int{1, 2, 5, 6, 23}
+	actualSlice, err := StringSliceToIntSlice(stringSlice)
+
+	// Check that slice successfully casted into []int slice
+	suite.Require().NoError(err)
+	suite.Require().Equal(expectedSlice, actualSlice)
+}
+
+func (suite *StringSliceToIntSliceTestSuite) TestNegativeData() {
+	// Prepare incorrect (for casting) slice for StringSliceToIntSlice() function
+	stringSlice := []string{"1", "2", "5", "6", "23", "someBadData"}
+	_, err := StringSliceToIntSlice(stringSlice)
+
+	// Verify that error is throws by StringSliceToIntSlice() function
+	suite.Require().Error(err)
+}
+
 func TestStringSliceToIntSliceTestSuite(t *testing.T) {
 	suite.Run(t, new(StringSliceToIntSliceTestSuite))
+}
+
+type RetryUntilSuccessfulTestSuite struct {
+	suite.Suite
+}
+
+func (suite *RetryUntilSuccessfulTestSuite) TestPositive() {
+	err := RetryUntilSuccessful(2*time.Second, 1*time.Second, func() bool {
+		return true
+	})
+
+	suite.Require().NoError(err)
+}
+
+func (suite *RetryUntilSuccessfulTestSuite) TestNegative() {
+	err := RetryUntilSuccessful(2*time.Second, 1*time.Second, func() bool {
+		return false
+	})
+
+	suite.Require().Error(err)
+
+}
+
+func (suite *RetryUntilSuccessfulTestSuite) TestNumberOfCalls() {
+	// Create actual and expected number of calls
+	actualNumberOfCalls := 0
+	expectedNumberOfCalls := 15
+
+	_ = RetryUntilSuccessful(15*time.Second, 1*time.Second, func() bool {
+		_, _, _, ok := runtime.Caller(1)
+		if ok {
+			actualNumberOfCalls++
+		}
+		return false
+	})
+
+	suite.Require().Equal(expectedNumberOfCalls, actualNumberOfCalls)
+}
+
+func (suite *RetryUntilSuccessfulTestSuite) TestTimeBetweenIntervals() {
+	// Starting time from currentTime - 1 cause function calls right now
+	startingIntervalTime := time.Now().Unix() - 1
+	_ = RetryUntilSuccessful(55*time.Second, 1*time.Second, func() bool {
+		_, _, _, ok := runtime.Caller(1)
+		if ok {
+			// If call was successfull create finishIntervalTime variable and set currentTime
+			finishIntervalTime := time.Now().Unix()
+			// Verify that difference betwen previous interval and current interval is 1
+			suite.Require().True(finishIntervalTime-startingIntervalTime == 1)
+			// Set currentInterval time value into previos interval variable
+			startingIntervalTime = finishIntervalTime
+		}
+		return false
+	})
+
+}
+
+func (suite *RetryUntilSuccessfulTestSuite) TestDurationTime() {
+	// Initialize startTime as currentTime
+	startTime := time.Now().Unix()
+	_ = RetryUntilSuccessful(10*time.Second, 1*time.Second, func() bool {
+		return false
+	})
+	// Initialize finishTime as currentTime
+	finishTime := time.Now().Unix()
+
+	// Verify that function duration is as expected
+	suite.Require().True(finishTime-startTime == 10)
 }
 
 func TestRetryUntilSuccessfulTestSuite(t *testing.T) {
