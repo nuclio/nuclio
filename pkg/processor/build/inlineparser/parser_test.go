f90e27663c6209bc8abdad02c5d9ce8dbe5bc71d
Eran Duchan <pavius@gmail.com>
2018-03-20 23:38:53 +0200

    Inlineparser space insensitive (#658)

--- pkg/processor/build/inlineparser/parser_test.go
+++ pkg/processor/build/inlineparser/parser_test.go
@@ -23,7 +23,6 @@ import (
 	"github.com/nuclio/logger"
 	"github.com/nuclio/zap"
 	"github.com/stretchr/testify/suite"
-	"gopkg.in/yaml.v2"
 )
 
 type InlineParserTestSuite struct {
@@ -45,17 +44,12 @@ func (suite *InlineParserTestSuite) TestValidBlockSingleChar() {
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# @nuclio.createFiles
+#      @nuclio.configure
 #
-# processor.yaml:
-#   function:
-#     kind: "python"
-#     python_version: "3"
-#     handler: parser:handler
-#
-# build.yaml:
-#   commands:
-#     - pip install simplejson
+#   function.yaml:
+#     spec:
+#       runtime: "python"
+#       handler: parser:handler
 #
 
 import simplejson
@@ -77,10 +71,47 @@ def handler(context, event):
 	blocks, err := suite.parser.Parse(tmpFile.Name())
 	suite.Require().NoError(err)
 
-	processorYaml := blocks["createFiles"]["processor.yaml"]
-	yaml.Marshal(processorYaml)
+	// get function YAML
+	suite.Require().NotEmpty(blocks["configure"])
+}
 
-	// TODO
+func (suite *InlineParserTestSuite) TestValidBlockSingleCharNoSpaces() {
+	content := `
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#@nuclio.configure
+#
+#function.yaml:
+#  spec:
+#    runtime: "python"
+#    handler: parser:handler
+#
+
+import simplejson
+
+def handler(context, event):
+    """Return a field from within a json"""
+
+    context.logger.info('Hello from Python')
+    body = simplejson.loads(event.body.decode('utf-8'))
+    return body['return_this']
+`
+	tmpFile, err := ioutil.TempFile("", "nucilio-parser-test")
+	suite.Require().NoError(err)
+	tmpFile.Close()
+
+	err = ioutil.WriteFile(tmpFile.Name(), []byte(content), 0600)
+	suite.Require().NoError(err)
+
+	blocks, err := suite.parser.Parse(tmpFile.Name())
+	suite.Require().NoError(err)
+
+	// get function YAML
+	suite.Require().NotEmpty(blocks["configure"])
 }
 
 func TestInlineParserTestSuite(t *testing.T) {
