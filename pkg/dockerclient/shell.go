f0078adb7a2bab7d18f1aa8aa0e39112413173d8
Omri Harel <omriharel@users.noreply.github.com>
2018-03-29 14:41:41 +0300

    whoops

--- pkg/dockerclient/shell.go
+++ pkg/dockerclient/shell.go
@@ -232,17 +232,20 @@ func (c *ShellClient) RunContainer(imageName string, runOptions *RunOptions) (st
 	stdoutLines := strings.Split(runResult.Output, "\n")
 	lastStdoutLine := c.getLastNonEmptyLine(stdoutLines, 0)
 
-	// make sure there are no spaces in the ID, as normally we expect this command to only produce container ID.
-	// if the image didn't exist prior to calling RunContainer, it will be pulled implicitly which will
-	// cause additional information to be outputted. if runOptions.ImageMayNotExist is false,
-	// this will result in an error.
-	if strings.Contains(lastStdoutLine, " ") && !runOptions.ImageMayNotExist {
-		return "", fmt.Errorf("Output from docker command includes more than just ID: %s", lastStdoutLine)
-	}
+	// make sure there are no spaces in the ID, as normally we expect this command to only produce container ID
+	if strings.Contains(lastStdoutLine, " ") {
 
-	// if the implicit image pull was allowed and actually happened, the container ID will appear in the
-	// second to last line ¯\_(ツ)_/¯
-	lastStdoutLine = c.getLastNonEmptyLine(stdoutLines, 1)
+		// if the image didn't exist prior to calling RunContainer, it will be pulled implicitly which will
+		// cause additional information to be outputted. if runOptions.ImageMayNotExist is false,
+		// this will result in an error.
+		if !runOptions.ImageMayNotExist {
+			return "", fmt.Errorf("Output from docker command includes more than just ID: %s", lastStdoutLine)
+		}
+
+		// if the implicit image pull was allowed and actually happened, the container ID will appear in the
+		// second to last line ¯\_(ツ)_/¯
+		lastStdoutLine = c.getLastNonEmptyLine(stdoutLines, 1)
+	}
 
 	return lastStdoutLine, err
 }
