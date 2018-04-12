ede94ecce95f35303225864ddc6d6161061265c6
Miki Tebeka <miki.tebeka@gmail.com>
2018-04-12 14:46:14 +0300

    start of dealer

--- pkg/processor/trigger/partitioned/kafka/test/kafka_test.go
+++ pkg/processor/trigger/partitioned/kafka/test/kafka_test.go
@@ -19,25 +19,71 @@ package test
 import (
 	"fmt"
 	"testing"
+	"time"
 
+	"github.com/nuclio/nuclio/pkg/dockerclient"
 	"github.com/nuclio/nuclio/pkg/functionconfig"
+	"github.com/nuclio/nuclio/pkg/platform"
 	"github.com/nuclio/nuclio/pkg/processor/trigger/test"
 
+	"github.com/Shopify/sarama"
 	"github.com/stretchr/testify/suite"
 )
 
-type triggerTestSuite struct {
-	*KafkaTestSuite
+const (
+	brokerPort = 9092
+)
+
+type testSuite struct {
+	*triggertest.AbstractBrokerSuite
+	broker    *sarama.Broker
+	brokerURL string
+	producer  sarama.SyncProducer
+	topic     string
 }
 
-func (suite *triggerTestSuite) TestReceiveRecords() {
+func newTestSuite() *testSuite {
+	newTestSuite := &testSuite{
+		topic: "test-topic",
+	}
+
+	newTestSuite.AbstractBrokerSuite = triggertest.NewAbstractBrokerSuite(newTestSuite)
+
+	return newTestSuite
+}
+
+func (suite *testSuite) SetupSuite() {
+	suite.AbstractBrokerSuite.SetupSuite()
+
+	suite.Logger.Info("Creating broker resources")
+
+	// create broker
+	suite.brokerURL = fmt.Sprintf("%s:%d", suite.BrokerHost, brokerPort)
+	suite.broker = sarama.NewBroker(suite.brokerURL)
+
+	brokerConfig := sarama.NewConfig()
+	brokerConfig.Version = sarama.V0_10_1_0
+
+	// connect to the broker
+	err := suite.broker.Open(brokerConfig)
+	suite.Require().NoError(err, "Failed to open broker")
+
+	err = suite.createTopic(suite.topic, 1)
+	suite.Require().NoError(err, "Failed to create topics")
+
+	// create a sync producer
+	suite.producer, err = sarama.NewSyncProducer([]string{suite.brokerURL}, nil)
+	suite.Require().NoError(err, "Failed to create sync producer")
+}
+
+func (suite *testSuite) TestReceiveRecords() {
 	createFunctionOptions := suite.GetDeployOptions("event_recorder", suite.FunctionPaths["python"])
 	createFunctionOptions.FunctionConfig.Spec.Triggers = map[string]functionconfig.Trigger{}
 	createFunctionOptions.FunctionConfig.Spec.Triggers["my-kafka"] = functionconfig.Trigger{
 		Kind: "kafka",
-		URL:  fmt.Sprintf("%s:9092", suite.BrokerHost),
+		URL:  suite.brokerURL,
 		Attributes: map[string]interface{}{
-			"topic":      suite.Topic,
+			"topic":      suite.topic,
 			"partitions": []int{0},
 		},
 	}
@@ -45,18 +91,113 @@ func (suite *triggerTestSuite) TestReceiveRecords() {
 	triggertest.InvokeEventRecorder(&suite.AbstractBrokerSuite.TestSuite,
 		suite.BrokerHost,
 		createFunctionOptions,
-		map[string]triggertest.TopicMessages{suite.Topic: {3}},
+		map[string]triggertest.TopicMessages{
+			suite.topic: {NumMessages: 3},
+		},
 		nil,
-		suite.PublishMessageToTopic)
+		suite.publishMessageToTopic)
 }
 
-// TestIntegrationSuite runs the suite
+func (suite *testSuite) TestDealer() {
+	/*
+		dealerTopic := "dealer-topic"
+
+		// init a create topic request
+		createTopicsRequest := sarama.CreateTopicsRequest{
+			TopicDetails: map[string]*sarama.TopicDetail{
+				dealerTopic: {
+					NumPartitions: 10,
+				},
+			},
+		}
+
+		// create topic
+		_, err := suite.broker.CreateTopics(&createTopicsRequest)
+		suite.Require().NoError(err, "Failed to create topics")
+
+		createFunctionOptions := suite.GetDeployOptions("event_recorder", suite.FunctionPaths["python"])
+		createFunctionOptions.FunctionConfig.Spec.Triggers = map[string]functionconfig.Trigger{}
+		createFunctionOptions.FunctionConfig.Spec.Triggers["my-kafka"] = functionconfig.Trigger{
+			Kind: "kafka",
+			URL:  suite.brokerURL,
+			Attributes: map[string]interface{}{
+				"topic":      dealerTopic,
+				"partitions": []int{0, 1},
+			},
+		}
+
+	*/
+	onAfterContainerRun := func(deployResult *platform.CreateFunctionResult) bool {
+		return true
+	}
+	fmt.Println("%v\n", onAfterContainerRun)
+
+	//	suite.DeployFunction(createFunctionOptions, onAfterContainerRun)
+}
+
+// GetContainerRunInfo returns information about the broker container
+func (suite *testSuite) GetContainerRunInfo() (string, *dockerclient.RunOptions) {
+	return "spotify/kafka", &dockerclient.RunOptions{
+		Ports: map[int]int{
+			2181:       2181,
+			brokerPort: brokerPort,
+		},
+		Env: map[string]string{
+			"ADVERTISED_HOST": suite.BrokerHost,
+			"ADVERTISED_PORT": fmt.Sprintf("%d", brokerPort),
+		},
+	}
+}
+
+// WaitForBroker waits until the broker is ready
+func (suite *testSuite) WaitForBroker() error {
+	var err error
+	timeout := 10 * time.Second
+
+	for start := time.Now(); time.Since(start) <= timeout; time.Sleep(100 * time.Millisecond) {
+		_, err = sarama.NewConsumer([]string{suite.brokerURL}, nil)
+		if err == nil {
+			return nil
+		}
+	}
+
+	return err
+}
+
+func (suite *testSuite) publishMessageToTopic(topic string, body string) error {
+	producerMessage := sarama.ProducerMessage{
+		Topic: topic,
+		Key:   sarama.StringEncoder("key"),
+		Value: sarama.StringEncoder(body),
+	}
+
+	suite.Logger.InfoWith("Producing")
+
+	partition, offset, err := suite.producer.SendMessage(&producerMessage)
+	suite.Require().NoError(err, "Failed to publish to queue")
+
+	suite.Logger.InfoWith("Produced", "partition", partition, "offset", offset)
+
+	return nil
+}
+
+func (suite *testSuite) createTopic(topic string, numPartitions int32) error {
+	createTopicsRequest := &sarama.CreateTopicsRequest{
+		TopicDetails: map[string]*sarama.TopicDetail{
+			topic: {
+				NumPartitions: numPartitions,
+			},
+		},
+	}
+
+	_, err := suite.broker.CreateTopics(createTopicsRequest)
+	return err
+}
+
 func TestIntegrationSuite(t *testing.T) {
 	if testing.Short() {
 		return
 	}
 
-	suiteInstance := &triggerTestSuite{}
-	suiteInstance.KafkaTestSuite = NewKafkaTestSuite("test-topic", 1, suiteInstance)
-	suite.Run(t, suiteInstance)
+	suite.Run(t, newTestSuite())
 }
