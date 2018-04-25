cc1954bde1798048e9a2e1991554a2b315e5007b
maksym-kyselytsia <maksym.kyselytsia@globallogic.com>
2018-04-25 16:31:19 +0300

    UI: Delivery 3-4 of bundle issues and tasks (#747)

--- pkg/dashboard/ui/src/app/app.route.js
+++ pkg/dashboard/ui/src/app/app.route.js
@@ -109,22 +109,21 @@
                                 if (!$stateParams.isNewFunction) {
                                     var functionMetadata = {
                                         name: $stateParams.functionId,
-                                        namespace: project.metadata.namespace
+                                        namespace: project.metadata.namespace,
+                                        projectName: project.metadata.name
                                     };
 
                                     return NuclioFunctionsDataService.getFunction(functionMetadata)
                                         .catch(function () {
                                             $state.go('app.project.functions', {projectId: $stateParams.projectId});
                                         });
-                                } else {
-                                    return $stateParams.functionData;
                                 }
-
                             })
-                                .catch(function (error) {
+                                .catch(function () {
                                     $state.go('app.projects');
                                 });
-                        }]
+                        }
+                    ]
                 }
             })
             .state('app.project.function.edit', {
@@ -134,6 +133,9 @@
                     'function': {
                         template: '<ncl-version data-version="$resolve.function"></ncl-version>'
                     }
+                },
+                params: {
+                    functionData: {}
                 },
                 data: {
                     pageTitle: 'Edit version',
