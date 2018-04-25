5625114cfd3a7d490f71954c6ccc435aa78a9f3e
Eran Duchan <pavius@gmail.com>
2018-04-25 11:11:13 +0300

    call onafterbuild on error as well

--- pkg/platform/abstract/platform.go
+++ pkg/platform/abstract/platform.go
@@ -94,16 +94,15 @@ func (ap *Platform) HandleDeployFunction(createFunctionOptions *platform.CreateF
 			OnAfterConfigUpdate: onAfterConfigUpdatedWrapper,
 		})
 
-		if buildErr != nil {
-			return nil, errors.Wrap(buildErr, "Failed to build image")
-		}
+		if buildErr == nil {
 
-		// use the function configuration augmented by the builder
-		createFunctionOptions.FunctionConfig.Spec.Image = buildResult.Image
+			// use the function configuration augmented by the builder
+			createFunctionOptions.FunctionConfig.Spec.Image = buildResult.Image
 
-		// if run registry isn't set, set it to that of the build
-		if createFunctionOptions.FunctionConfig.Spec.RunRegistry == "" {
-			createFunctionOptions.FunctionConfig.Spec.RunRegistry = createFunctionOptions.FunctionConfig.Spec.Build.Registry
+			// if run registry isn't set, set it to that of the build
+			if createFunctionOptions.FunctionConfig.Spec.RunRegistry == "" {
+				createFunctionOptions.FunctionConfig.Spec.RunRegistry = createFunctionOptions.FunctionConfig.Spec.Build.Registry
+			}
 		}
 	} else {
 
@@ -120,7 +119,7 @@ func (ap *Platform) HandleDeployFunction(createFunctionOptions *platform.CreateF
 
 	// wrap the deployer's deploy with the base HandleDeployFunction
 	deployResult, err := onAfterBuild(buildResult, buildErr)
-	if err != nil {
+	if buildErr != nil || err != nil {
 		return nil, errors.Wrap(err, "Failed to deploy function")
 	}
 
