855bb045258dc9e95033235ef894256d0a804e8f
Miki Tebeka <miki.tebeka@gmail.com>
2018-06-07 16:31:17 +0300

    more job->trigger and just ignore non partitioned triggers

--- pkg/processor/webadmin/dealer/dealer.go
+++ pkg/processor/webadmin/dealer/dealer.go
@@ -106,13 +106,13 @@ func (d *Dealer) Post(w http.ResponseWriter, r *http.Request) {
 
 	reply := d.createReply()
 	triggers := d.processor.GetTriggers()
-	for jobID, job := range dealerRequest.Triggers {
-		triggerInstance, triggerFound := triggers[jobID]
+	for triggerID, trigger := range dealerRequest.Triggers {
+		triggerInstance, triggerFound := triggers[triggerID]
 
 		// Create new trigger
-		if !triggerFound && !job.Disable {
-			d.logger.InfoWith("Creating new trigger", "id", jobID, "config", job)
-			if err := d.createTrigger(jobID, job); err != nil {
+		if !triggerFound && !trigger.Disable {
+			d.logger.InfoWith("Creating new trigger", "id", triggerID, "config", trigger)
+			if err := d.createTrigger(triggerID, trigger); err != nil {
 				d.writeError(w, http.StatusInternalServerError, err)
 				return
 			}
@@ -120,38 +120,37 @@ func (d *Dealer) Post(w http.ResponseWriter, r *http.Request) {
 		}
 
 		if !triggerFound {
-			err := errors.Errorf("unknown trigger - %q", jobID)
+			err := errors.Errorf("unknown trigger - %q", triggerID)
 			d.writeError(w, http.StatusBadRequest, err)
 			return
 		}
 
 		stream, isStream := triggerInstance.(partitioned.Stream)
 		if !isStream {
-			err := errors.Errorf("job %q is not partitioned", jobID)
-			d.writeError(w, http.StatusBadRequest, err)
-			return
+			d.logger.WarnWith("trigger is not partitioned", "id", triggerID)
+			continue
 		}
 
 		// Stop trigger
-		if job.Disable {
-			d.logger.InfoWith("Disabling trigger", "id", jobID)
-			jobCheckpoint, err := d.processor.RemoveTrigger(jobID)
+		if trigger.Disable {
+			d.logger.InfoWith("Disabling trigger", "id", triggerID)
+			triggerCheckpoint, err := d.processor.RemoveTrigger(triggerID)
 			if err != nil {
-				httpError := errors.Wrapf(err, "Can't stop trigger %v", jobID)
+				httpError := errors.Wrapf(err, "Can't stop trigger %v", triggerID)
 				d.writeError(w, http.StatusBadRequest, httpError)
 				return
 			}
 
 			// We add this task to the reply since it won't be in the processor
 			// triggers anymore
-			tasks := d.jobCheckpointToTasks(jobCheckpoint)
-			reply.Triggers[jobID] = newTrigger(tasks, len(tasks), true)
+			tasks := d.triggerCheckpointToTasks(triggerCheckpoint)
+			reply.Triggers[triggerID] = newTrigger(tasks, len(tasks), true)
 			continue
 		}
 
 		partitions := stream.GetPartitions()
 		var deletedTasks []Task
-		for _, task := range job.Tasks {
+		for _, task := range trigger.Tasks {
 			partitionConfig := &functionconfig.Partition{
 				Checkpoint: &task.Checkpoint,
 				ID:         fmt.Sprintf("%d", task.ID),
@@ -161,7 +160,7 @@ func (d *Dealer) Post(w http.ResponseWriter, r *http.Request) {
 
 			// Create new partition
 			if !partitionFound && d.isRunState(task.State) {
-				d.logger.InfoWith("Adding partition", "trigger", jobID, "config", partitionConfig)
+				d.logger.InfoWith("Adding partition", "trigger", triggerID, "config", partitionConfig)
 				if err := triggerInstance.AddPartition(partitionConfig); err != nil {
 					d.writeError(w, http.StatusInternalServerError, err)
 					return
@@ -170,7 +169,7 @@ func (d *Dealer) Post(w http.ResponseWriter, r *http.Request) {
 			}
 
 			if !partitionFound {
-				err := errors.Errorf("Partition %v not found in trigger %v", task.ID, jobID)
+				err := errors.Errorf("Partition %v not found in trigger %v", task.ID, triggerID)
 				d.writeError(w, http.StatusBadRequest, err)
 				return
 			}
@@ -182,15 +181,15 @@ func (d *Dealer) Post(w http.ResponseWriter, r *http.Request) {
 			}
 
 			if !d.isStopState(task.State) {
-				err := errors.Errorf("Trigger %v, Task %v - unknown action %v", jobID, task.ID, task.State)
+				err := errors.Errorf("Trigger %v, Task %v - unknown action %v", triggerID, task.ID, task.State)
 				d.writeError(w, http.StatusBadRequest, err)
 				return
 			}
 
-			d.logger.InfoWith("Removing task", "job", jobID, "task", task.ID)
+			d.logger.InfoWith("Removing task", "trigger", triggerID, "task", task.ID)
 			checkpoint, err := triggerInstance.RemovePartition(partitionConfig)
 			if err != nil {
-				httpError := errors.Wrapf(err, "Can't delete task %v from job %v", task.ID, jobID)
+				httpError := errors.Wrapf(err, "Can't delete task %v from trigger %v", task.ID, triggerID)
 				d.writeError(w, http.StatusInternalServerError, httpError)
 				return
 			}
@@ -206,7 +205,7 @@ func (d *Dealer) Post(w http.ResponseWriter, r *http.Request) {
 		tasks := d.streamTasks(stream)
 
 		tasks = append(tasks, deletedTasks...)
-		reply.Triggers[jobID] = newTrigger(tasks, len(tasks)-len(deletedTasks), false)
+		reply.Triggers[triggerID] = newTrigger(tasks, len(tasks)-len(deletedTasks), false)
 	}
 
 	d.addMissingTasks(triggers, reply)
@@ -284,16 +283,16 @@ func (d *Dealer) addMissingTasks(triggers map[string]trigger.Trigger, reply *Mes
 	}
 }
 
-func (d *Dealer) createTrigger(jobID string, job *Trigger) error {
+func (d *Dealer) createTrigger(triggerID string, trigger *Trigger) error {
 	processorConfig := d.processor.GetConfiguration()
-	triggerConfig, ok := processorConfig.Spec.Triggers[jobID]
+	triggerConfig, ok := processorConfig.Spec.Triggers[triggerID]
 
 	// TODO: Support new triggers on-the-fly
 	if !ok {
-		return errors.Errorf("Can't find configuration for trigger %s", jobID)
+		return errors.Errorf("Can't find configuration for trigger %s", triggerID)
 	}
 
-	if err := d.processor.CreateTrigger(jobID, processorConfig, triggerConfig); err != nil {
+	if err := d.processor.CreateTrigger(triggerID, processorConfig, triggerConfig); err != nil {
 		return errors.Wrap(err, "Can't create trigger")
 	}
 
@@ -342,14 +341,14 @@ func (d *Dealer) writeError(w http.ResponseWriter, status int, err error) {
 	})
 }
 
-func (d *Dealer) jobCheckpointToTasks(jobCheckpoint functionconfig.Checkpoint) []Task {
-	if jobCheckpoint == nil {
+func (d *Dealer) triggerCheckpointToTasks(triggerCheckpoint functionconfig.Checkpoint) []Task {
+	if triggerCheckpoint == nil {
 		return nil
 	}
 
 	checkpoints := make(map[int]functionconfig.Checkpoint)
-	if err := json.Unmarshal([]byte(*jobCheckpoint), &checkpoints); err != nil {
-		d.logger.WarnWith("Can't decode job checkpoints", "error", err)
+	if err := json.Unmarshal([]byte(*triggerCheckpoint), &checkpoints); err != nil {
+		d.logger.WarnWith("Can't decode trigger checkpoints", "error", err)
 		return nil
 	}
 
