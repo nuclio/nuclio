cc2bf24f408f33cfd33e1295fb5d2f40e5eb2551
Miki Tebeka <miki.tebeka@gmail.com>
2018-06-11 09:50:02 +0300

    Low resolution clock

---
+++ pkg/processor/util/clock/clock.go
@@ -0,0 +1,54 @@
+/*
+Copyright 2017 The Nuclio Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package clock
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+// Clock is a low resulution clock. It uses less resources and is faster than calling
+// time.Now
+type Clock struct {
+	// Resolution is the clock resolution
+	Resolution time.Duration
+
+	now atomic.Value
+}
+
+// New returns a new clock with desired resolution
+func New(resolution time.Duration) *Clock {
+	clock := &Clock{
+		Resolution: resolution,
+	}
+	clock.now.Store(time.Now())
+
+	go clock.tick()
+	return clock
+}
+
+// Now returns the curren time
+func (c *Clock) Now() time.Time {
+	return c.now.Load().(time.Time)
+}
+
+func (c *Clock) tick() {
+	for {
+		time.Sleep(c.Resolution)
+		c.now.Store(time.Now())
+	}
+}
