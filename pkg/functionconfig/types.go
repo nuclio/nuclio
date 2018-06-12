f3da20fa22036c898c923d979a01ff1c6b797479
Omri Harel <omriharel@users.noreply.github.com>
2018-06-12 16:30:43 +0300

    fix runtimeAttributes typo

--- pkg/functionconfig/types.go
+++ pkg/functionconfig/types.go
@@ -157,7 +157,7 @@ type Build struct {
 	Dependencies       []string               `json:"dependencies,omitempty"`
 	OnbuildImage       string                 `json:"onbuildImage,omitempty"`
 	Offline            bool                   `json:"offline,omitempty"`
-	RuntimeAttributes  map[string]interface{} `json:"offline,omitempty"`
+	RuntimeAttributes  map[string]interface{} `json:"runtimeAttributes,omitempty"`
 }
 
 // Spec holds all parameters related to a function's configuration
