b342e1be94ea607c926f47122a11b973ee80b648
maksym.kyselytsia <maksym.kyselytsia@globallogic.com>
2018-07-13 18:05:01 +0300

    new test event pane

--- pkg/dashboard/ui/src/app/app.run.js
+++ pkg/dashboard/ui/src/app/app.run.js
@@ -4,7 +4,7 @@
     angular.module('nuclio.app')
         .run(appInit);
 
-    function appInit($rootScope, $urlRouter, $http, $httpBackend, $injector, lodash, ConfigService) {
+    function appInit($rootScope, $urlRouter, $http, $httpBackend, $injector, lodash, ConfigService, DialogsService, NuclioProjectsDataService) {
         // @if !IGZ_TESTING
         $rootScope.$on('$locationChangeSuccess', function (event) {
             // @if IGZ_E2E_TESTING
@@ -22,6 +22,20 @@
                 })
                 .then(function (config) {
                     lodash.merge(ConfigService, config.data);
+                })
+                .then(function () {
+                    NuclioProjectsDataService.getExternalIPAddresses()
+                        .then(function (response) {
+                            ConfigService.externalIPAddress = response.data.externalIPAddresses.addresses[0];
+                        })
+                        .catch(function () {
+                            DialogsService.alert('Oops: Unknown error occurred while retrieving external IP address');
+                        });
+                })
+                .catch(angular.noop)
+                .then(function () {
+
+                    // continue with the event that was stopped before with `event.preventDefault()`
                     $urlRouter.sync();
                 });
         });
