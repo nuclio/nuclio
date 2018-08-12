a9a3adb2b2b2bfc6573c9b9df7eaff3067fed546
Andrew Mavdryk <andrii.mavdryk@globallogic.com>
2018-08-12 15:15:07 +0300

    UI: Add left navigation menu + namespaces (#906)

---
+++ pkg/dashboard/ui/src/app/components/main-side-menu/main-side-menu.component.js
@@ -0,0 +1,71 @@
+(function () {
+    'use strict';
+
+    angular.module('nuclio.app')
+        .component('nclMainSideMenu', {
+            templateUrl: 'main-side-menu/main-side-menu.tpl.html',
+            controller: NclMainSideMenuController
+        });
+
+    function NclMainSideMenuController($state, lodash, ConfigService, NuclioVersionService,
+                                       NuclioNamespacesDataService) {
+        var ctrl = this;
+
+        ctrl.$onInit = onInit;
+
+        ctrl.isDemoMode = ConfigService.isDemoMode;
+        ctrl.namespaceData = NuclioNamespacesDataService.namespaceData;
+
+        ctrl.isNamespacesExist = isNamespacesExist;
+        ctrl.isActiveState = isActiveState;
+        ctrl.onDataChange = onDataChange;
+
+        //
+        // Hook methods
+        //
+
+        /**
+         * Initialization function
+         */
+        function onInit() {
+            NuclioVersionService.getVersion()
+                .then(function (response) {
+                    ctrl.nuclioVersion = lodash.get(response.data, 'dashboard.label', 'unstable');
+                });
+        }
+
+        //
+        // Public method
+        //
+
+        /**
+         * Checks if namespaces exist
+         * @returns {boolean}
+         */
+        function isNamespacesExist() {
+            return !lodash.isEmpty(ctrl.namespaceData.namespaces);
+        }
+
+        /**
+         * Checks if current state is active
+         * @param {Array.<string>} states
+         * @returns {boolean}
+         */
+        function isActiveState(states) {
+            var activeStates = lodash.chain(states).map($state.includes).without(false).value();
+
+            return !lodash.isEmpty(activeStates);
+        }
+
+        /**
+         * Callback on select item in namespace dropdown
+         * @param {Object} item - selected item
+         */
+        function onDataChange(item) {
+            ctrl.namespaceData.selectedNamespace = item;
+            localStorage.setItem('namespace', item.name);
+
+            $state.go('app.projects', {namespace: item.name});
+        }
+    }
+}());
