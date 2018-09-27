df54e480a5677b67d7aaa73c859c6202085ca65e
maksym-kyselytsia <maksym.kyselytsia@globallogic.com>
2018-09-27 22:15:20 +0300

    Impl IG-1967 [General] Upgrade to AngularJs v1.7 (#960)

--- pkg/dashboard/ui/src/app/app.config.js
+++ pkg/dashboard/ui/src/app/app.config.js
@@ -22,13 +22,10 @@
         .config(scrollBarsConfig)
         .config(lodashConfig);
 
-    function config($compileProvider, $locationProvider, $httpProvider, $qProvider) {
+    function config($locationProvider, $httpProvider, $qProvider) {
         $locationProvider.html5Mode(true);
 
         $httpProvider.defaults.withCredentials = true;
-
-        // allows to get values from bindings outside of onInit function (since angular 1.6)
-        $compileProvider.preAssignBindingsEnabled(true);
 
         // prevents 'Possibly unhandled rejection' error
         $qProvider.errorOnUnhandledRejections(false);
