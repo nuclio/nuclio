7a1f8e73a694c8af169915d33a21db12cf7c0958
Eran Nussbaum <eran-nussbaum@users.noreply.github.com>
2018-10-04 16:47:04 +0300

    UI: Remove falsely displayed warning dialog (on new function)

--- pkg/dashboard/ui/src/app/app.run.js
+++ pkg/dashboard/ui/src/app/app.run.js
@@ -4,42 +4,36 @@
     angular.module('nuclio.app')
         .run(appInit);
 
-    function appInit($rootScope, $state, $urlRouter, $http, $injector, lodash, ConfigService,
-                     NuclioProjectsDataService) {
+    function appInit($urlRouter, $http, $injector, lodash, ConfigService, NuclioProjectsDataService) {
         // @if !IGZ_TESTING
-        $rootScope.$on('$locationChangeSuccess', function (event) {
-            // @if IGZ_E2E_TESTING
-            if ($injector.has('$httpBackend')) {
-                var httpBackend = $injector.get('$httpBackend');
-                httpBackend.whenGET(/dashboard-config\.json$/).passThrough();
-            }
+        // @if IGZ_E2E_TESTING
+        if ($injector.has('$httpBackend')) {
+            var httpBackend = $injector.get('$httpBackend');
+            httpBackend.whenGET(/dashboard-config\.json$/).passThrough();
+        }
 
-            // @endif
-            if ($state.current.name === '') {
-                event.preventDefault();
-                $http
-                    .get('/dashboard-config.json', {
-                        responseType: 'json',
-                        headers: {'Cache-Control': 'no-cache'}
+        // @endif
+        $http
+            .get('/dashboard-config.json', {
+                responseType: 'json',
+                headers: {'Cache-Control': 'no-cache'}
+            })
+            .then(function (config) {
+                lodash.merge(ConfigService, config.data);
+            })
+            .then(function () {
+                NuclioProjectsDataService.getExternalIPAddresses()
+                    .then(function (response) {
+                        ConfigService.externalIPAddress = response.externalIPAddresses.addresses[0];
                     })
-                    .then(function (config) {
-                        lodash.merge(ConfigService, config.data);
-                    })
-                    .then(function () {
-                        NuclioProjectsDataService.getExternalIPAddresses()
-                            .then(function (response) {
-                                ConfigService.externalIPAddress = response.externalIPAddresses.addresses[0];
-                            })
-                            .catch(function () {
-                                ConfigService.externalIPAddress = null;
-                            });
-                    })
-                    .then(function () {
-                        $urlRouter.listen();
-                        $urlRouter.sync();
+                    .catch(function () {
+                        ConfigService.externalIPAddress = null;
                     });
-            }
-        });
+            })
+            .then(function () {
+                $urlRouter.listen();
+                $urlRouter.sync();
+            });
         // @endif
 
         /*eslint angular/on-watch: 0*/
