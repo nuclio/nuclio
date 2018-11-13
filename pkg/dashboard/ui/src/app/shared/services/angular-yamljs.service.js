529a739dc4d4708c0cc461a11ad9a02611c62fcf
vadym.yarovoi <vadym.yarovoi@globallogic.com>
2018-11-13 14:06:18 +0200

    IG-2017 [Nuclio] Add "Export" button to project screen

--- pkg/dashboard/ui/src/app/shared/services/angular-yamljs.service.js
+++
@@ -1,6 +0,0 @@
-angular.module('angular-yamljs', [])
-    .provider('YAML', function () {
-        this.$get = ['$window', function ($window) {
-            return $window.YAML;
-        }];
-    });
