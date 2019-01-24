3ded1e81265bb03edd27b9bcad9fe5052424394d
Lev <levr@iguaz.io>
2019-01-24 16:06:18 +0200

    refactor to resource scaler plugin

--- cmd/dashboard/app/dashboard.go
+++ cmd/dashboard/app/dashboard.go
@@ -55,7 +55,7 @@ func Run(listenAddress string,
 	}
 
 	// create a root logger
-	rootLogger, err := loggersink.CreateSystemLogger("controller", platformConfiguration)
+	rootLogger, err := loggersink.CreateSystemLogger("dashboard", platformConfiguration)
 	if err != nil {
 		return errors.Wrap(err, "Failed to create logger")
 	}
