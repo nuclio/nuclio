4af5d358f4749e1bae9c6bc22b3834f865b79ea3
Lev Radomislensky <lev.radomislensky@gmail.com>
2019-02-19 18:06:26 +0200

    Fix issue with v3io partition not existing when creating trigger (#1150)

--- pkg/processor/trigger/partitioned/v3io/partition.go
+++ pkg/processor/trigger/partitioned/v3io/partition.go
@@ -18,6 +18,8 @@ package v3io
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/nuclio/nuclio/pkg/errors"
@@ -26,6 +28,8 @@ import (
 	"github.com/nuclio/logger"
 	v3iohttp "github.com/v3io/v3io-go-http"
 )
+
+const NotFoundError = "Failed GET with status 404"
 
 type partition struct {
 	*partitioned.AbstractPartition
@@ -55,7 +59,13 @@ func newPartition(parentLogger logger.Logger, v3ioTrigger *v3io, partitionID int
 }
 
 func (p *partition) Read() error {
-	partitionPath := fmt.Sprintf("%s/%d", p.v3ioTrigger.streamPath, p.partitionID)
+	partitionPath := fmt.Sprintf("%s/%d", strings.TrimRight(p.v3ioTrigger.streamPath, "/"), p.partitionID)
+	pollingInterval := time.Duration(p.v3ioTrigger.configuration.PollingIntervalMs) * time.Millisecond
+
+	err := p.waitPartitionAvailable(partitionPath, pollingInterval)
+	if err != nil {
+		return errors.Wrap(err, "Failed to wait for partition availability")
+	}
 
 	location, err := p.seek(partitionPath)
 	if err != nil {
@@ -65,8 +75,6 @@ func (p *partition) Read() error {
 	p.Logger.DebugWith("Starting to read from partition",
 		"location", location,
 		"pollingInterval", p.v3ioTrigger.configuration.PollingIntervalMs)
-
-	pollingInterval := time.Duration(p.v3ioTrigger.configuration.PollingIntervalMs) * time.Millisecond
 
 	for {
 		getRecordsOutput, err := p.getRecords(partitionPath, location, pollingInterval)
@@ -129,5 +137,49 @@ func (p *partition) getRecords(partitionPath string, location string, pollingInt
 	response.Release()
 
 	return response.Output.(*v3iohttp.GetRecordsOutput), nil
+}
 
+func (p *partition) waitPartitionAvailable(partitionPath string, pollingInterval time.Duration) error {
+	for {
+		listBucketResult, err := p.getListBucket()
+
+		if err != nil {
+
+			// print only if error is something else other than 404
+			if err.Error() != NotFoundError {
+				p.Logger.ErrorWith("Failed to list bucket", "err", err)
+			}
+			time.Sleep(pollingInterval)
+			continue
+		}
+
+		// look to see if the partition inside the stream path
+		for _, partition := range listBucketResult.Contents {
+			splittedPartitionKey := strings.Split(strings.TrimRight(partition.Key, "/"), "/")
+			if splittedPartitionKey[len(splittedPartitionKey)-1] == strconv.Itoa(p.partitionID) {
+				return nil
+			}
+		}
+		time.Sleep(pollingInterval)
+	}
+}
+
+func (p *partition) getListBucket() (*v3iohttp.ListBucketOutput, error) {
+	request := &v3iohttp.ListBucketInput{
+		Path: p.v3ioTrigger.streamPath,
+	}
+	response, err := p.v3ioTrigger.container.Sync.ListBucket(request)
+
+	// upon errors, response is being released by ListBucket
+	if err != nil {
+		return nil, err
+	}
+	response.Release()
+
+	listBucketResult, ok := response.Output.(*v3iohttp.ListBucketOutput)
+	if !ok {
+		return nil, errors.New("Failed to cast response to ListBucketOutput")
+	}
+
+	return listBucketResult, nil
 }
