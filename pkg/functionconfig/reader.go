10a00b406852b2d4ef2cda2b7ddf7c213e922282
sahare92 <sahare92@gmail.com>
2019-04-29 09:03:58 +0300

    CR2 fixes

--- pkg/functionconfig/reader.go
+++ pkg/functionconfig/reader.go
@@ -40,10 +40,10 @@ func NewReader(parentLogger logger.Logger) (*Reader, error) {
 }
 
 // Merges codeEntry config with the function config.
-// Config populated values won't get override by codeEntry values.
+// CodeEntry config will get overridden by config values.
 // Enriches config with env vars existing only in codeEntry config
 func (r *Reader) Read(reader io.Reader, configType string, config *Config) error {
-	var codeEntryConfigMap, configMap map[string]interface{}
+	var codeEntryConfigAsMap, configAsMap map[string]interface{}
 	var codeEntryConfig Config
 
 	bodyBytes, err := ioutil.ReadAll(reader)
@@ -66,34 +66,34 @@ func (r *Reader) Read(reader io.Reader, configType string, config *Config) error
 		}
 	}
 
-	if err = yaml.Unmarshal(bodyBytes, &codeEntryConfigMap); err != nil {
+	if err = yaml.Unmarshal(bodyBytes, &codeEntryConfigAsMap); err != nil {
 		return errors.Wrap(err, "Failed to parse received config")
 	}
 
 	// parse config to JSON - in order to parse it afterwards into a map
-	configJSON, err := json.Marshal(config)
+	configAsJSON, err := json.Marshal(config)
 	if err != nil {
 		return errors.Wrap(err, "Failed to parse config to JSON")
 	}
 
 	// create a map from the JSON of the config. we need it as a map so we will be able to use mergo.Merge()
-	if err = json.Unmarshal(configJSON, &configMap); err != nil {
+	if err = json.Unmarshal(configAsJSON, &configAsMap); err != nil {
 		return errors.Wrap(err, "Failed to parse config as JSON to map")
 	}
 
-	// merge config with codeEntry config - config populated values won't get overridden by codeEntry config values
-	if err = mergo.Merge(&configMap, &codeEntryConfigMap); err != nil {
+	// merge config with codeEntry config - config populated values will take precedence
+	if err = mergo.Merge(&configAsMap, &codeEntryConfigAsMap); err != nil {
 		return errors.Wrap(err, "Failed to merge config and codeEntry config")
 	}
 
-	// parse the modified config map to be as JSON, so it can be easily unmarshalled into the config struct
-	mergedConfigJSON, err := json.Marshal(configMap)
+	// parse the modified configAsMap to be as JSON, so it can be easily unmarshalled into the config struct
+	mergedConfigAsJSON, err := json.Marshal(configAsMap)
 	if err != nil {
 		return errors.Wrap(err, "Failed to parse new config from from map to JSON")
 	}
 
 	// load merged config into the function config
-	if err = json.Unmarshal(mergedConfigJSON, &config); err != nil {
+	if err = json.Unmarshal(mergedConfigAsJSON, &config); err != nil {
 		return errors.Wrap(err, "Failed to parse new config from JSON to *Config struct")
 	}
 
