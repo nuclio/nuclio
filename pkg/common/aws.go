03e698f49023b893bc8bb29d16a93845011f9462
sahare92 <sahare92@gmail.com>
2019-06-03 19:23:01 +0300

    S3 code entry type (#1242)

---
+++ pkg/common/aws.go
@@ -0,0 +1,48 @@
+package common
+
+import (
+	"os"
+	"path"
+	"path/filepath"
+
+	"github.com/nuclio/nuclio/pkg/errors"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+func DownloadFileFromAWSS3(file *os.File, bucket, itemKey, region, accessKeyID, secretAccessKey, sessionToken string) error {
+	itemKey = filepath.Clean(itemKey)
+
+	pathInsideBucket, item := path.Split(itemKey)
+	bucketAndPath := path.Join(bucket, pathInsideBucket) + "/"
+
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String("us-east-1"), // default region (some valid region must be mentioned)
+		Credentials: credentials.NewStaticCredentials(accessKeyID, secretAccessKey, sessionToken),
+	})
+	if err != nil {
+		return errors.Wrap(err, "Failed to create AWS session")
+	}
+
+	// get the bucket's region in case it wasn't given
+	if region == "" {
+		region, err = s3manager.GetBucketRegion(aws.BackgroundContext(), sess, bucketAndPath, "")
+		if err != nil {
+			return errors.Wrap(err, "Failed to get bucket region")
+		}
+	}
+	sess.Config.Region = aws.String(region)
+
+	downloader := s3manager.NewDownloader(sess)
+	_, err = downloader.Download(file,
+		&s3.GetObjectInput{
+			Bucket: aws.String(bucketAndPath),
+			Key:    aws.String(item),
+		})
+
+	return err
+}
