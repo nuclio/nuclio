3e73bbcc2b211bb65bf46ea7ec4d125e4214b378
Hedingber <hedingber@gmail.com>
2019-09-10 18:23:17 +0300

    just return don't save the value

--- pkg/platform/kube/apis/nuclio.io/v1beta1/types.go
+++ pkg/platform/kube/apis/nuclio.io/v1beta1/types.go
@@ -20,105 +20,94 @@ type NuclioFunction struct {
 }
 
 func (nf *NuclioFunction) GetComputedReplicas() *int32 {
-	var replicas *int32
 	zero := int32(0)
 	one := int32(1)
 
 	if nf.Spec.Disabled || nf.Status.State == functionconfig.FunctionStateScaledToZero {
-		replicas = &zero
+		return &zero
 	} else if nf.Spec.Replicas != nil {
 
 		// Negative values -> 0
 		if *nf.Spec.Replicas < 0 {
-			replicas = &zero
+			return &zero
 		} else {
-			replicas = nf.Spec.Replicas
+			return nf.Spec.Replicas
 		}
 	} else {
 
-		// If the user hasn't specified desired replicas - base on the MinReplicas
-		minReplicas := nf.GetComputedMinReplicas()
+		// The user hasn't specified desired replicas
+		// If the function doesn't have resources yet (creating/scaling up from zero) - base on the MinReplicas or default to 1
+		if nf.Status.State == functionconfig.FunctionStateWaitingForResourceConfiguration {
+			minReplicas := nf.GetComputedMinReplicas()
 
-		if minReplicas > 0 {
-			replicas = &minReplicas
+			if minReplicas > 0 {
+				return &minReplicas
+			} else {
+				return &one
+			}
 		} else {
 
-			// If the function doesn't have resources yet (creating/scaling up from zero) - start from 1
-			if nf.Status.State == functionconfig.FunctionStateWaitingForResourceConfiguration {
-				replicas = &one
-			} else {
-
-				// Should get here only in case of update of an existing deployment,
-				// sending nil meaning leave the existing replicas as is
-				replicas = nil
-			}
+			// Should get here only in case of update of an existing deployment,
+			// sending nil meaning leave the existing replicas as is
+			return nil
 		}
 	}
-
-	return replicas
-
 }
 
 func (nf *NuclioFunction) GetComputedMinReplicas() int32 {
-	var minReplicas int32
 
 	// Replicas takes precedence over MinReplicas, so if given override with its value
 	if nf.Spec.Replicas != nil {
 
 		// Negative values -> 0
 		if *nf.Spec.Replicas < 0 {
-			minReplicas = 0
+			return 0
 		} else {
-			minReplicas = *nf.Spec.Replicas
+			return *nf.Spec.Replicas
 		}
 	} else {
 		if nf.Spec.MinReplicas != nil {
 
 			// Negative values -> 0
 			if *nf.Spec.MinReplicas < 0 {
-				minReplicas = 0
+				return 0
 			} else {
-				minReplicas = *nf.Spec.MinReplicas
+				return *nf.Spec.MinReplicas
 			}
 		} else {
 
 			// If neither Replicas nor MinReplicas is given, default to 1
-			minReplicas = 1
+			return 1
 		}
 	}
-
-	return minReplicas
 }
 
 func (nf *NuclioFunction) GetComputedMaxReplicas() int32 {
-	var maxReplicas int32
 
 	// Replicas takes precedence over MaxReplicas, so if given override with its value
 	if nf.Spec.Replicas != nil {
 
 		// Negative values -> 0
 		if *nf.Spec.Replicas <= 0 {
-			maxReplicas = 0
+			return 0
 		} else {
-			maxReplicas = *nf.Spec.Replicas
+			return *nf.Spec.Replicas
 		}
 	} else {
 		if nf.Spec.MaxReplicas != nil {
 
 			// Negative values -> 0
 			if *nf.Spec.MaxReplicas <= 0 {
-				maxReplicas = 0
+				return 0
 			} else {
-				maxReplicas = *nf.Spec.MaxReplicas
+				return *nf.Spec.MaxReplicas
 			}
 		} else {
 
 			// If neither Replicas nor MaxReplicas is given, default to 1
-			maxReplicas = 1
+			return 1
 		}
 	}
-
-	return maxReplicas
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
