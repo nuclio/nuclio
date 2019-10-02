1aba44ce8d357df1b562e9eec78af18237382edb
Gal Topper <galt@iguaz.io>
2019-10-02 16:20:34 +0300

    Remove accidentally added dependency.

--- vendor/github.com/vmware/vmware-go-kcl/support/scripts/ci.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run only the integration tests
-# go test -race ./test
-echo "Warning: Cannot find a good way to inject AWS credential to hmake container"
-echo "Don't use hmake ci. Use the following command directly"
-echo "go test -race ./test"
--- vendor/github.com/vmware/vmware-go-kcl/support/scripts/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-. support/scripts/functions.sh
-
-# Run only the unit tests and not integration tests
-go test -cover -race $(local_go_pkgs)
