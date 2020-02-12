3c00ba042b00ed88de60848b4741bc3abb96ec81
sahare92 <sahare92@gmail.com>
2020-02-12 14:42:49 +0200

    match known failures by substring instead of prefix

--- pkg/platform/abstract/platform.go
+++ pkg/platform/abstract/platform.go
@@ -584,7 +584,7 @@ func (ap *Platform) getLogLineAdditionalKwargs(log []byte) (map[string]string, e
 }
 
 func (ap *Platform) shouldAddToBriefErrorsMessage(logLevel uint8, logMessage string) bool {
-	knownFailurePrefixes := [...]string{"Failed to connect to broker"}
+	knownFailureSubstrings := [...]string{"Failed to connect to broker"}
 
 	// when log level is warning or above
 	if logLevel != 'D' && logLevel != 'I' {
@@ -592,8 +592,8 @@ func (ap *Platform) shouldAddToBriefErrorsMessage(logLevel uint8, logMessage str
 	}
 
 	// when the log message contains a known failure prefix
-	for _, prefix := range knownFailurePrefixes {
-		if strings.HasPrefix(logMessage, prefix) {
+	for _, prefix := range knownFailureSubstrings {
+		if strings.Contains(logMessage, prefix) {
 			return true
 		}
 	}
