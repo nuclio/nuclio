d076e80bac1a599e93f7758a387811e29e97b53f
sahare92 <sahare92@gmail.com>
2020-03-08 19:31:30 +0200

    lint

--- pkg/common/consts.go
+++ pkg/common/consts.go
@@ -4,5 +4,5 @@ type ReusedMessage string
 
 const (
 	UnexpectedTerminationChildProcess ReusedMessage = "Unexpected termination of child process"
-	FailedReadFromConnection ReusedMessage = "Failed to read from connection"
+	FailedReadFromConnection          ReusedMessage = "Failed to read from connection"
 )
