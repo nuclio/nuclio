71ecf57743a5ea4abb37500682d923146b75c597
LiranBG <liranbg@gmail.com>
2020-03-25 15:04:49 +0200

    move to parallel

---
+++ cmd/dashboard/docker/runner.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+_term() {
+  echo "Signal caught ... cleaning up"
+  kill -TERM "$child" 2>/dev/null
+}
+
+trap _term SIGTERM
+trap _term SIGINT
+
+echo "Running in parallel"
+
+parallel --citation
+
+# buffer output on line basis
+# exit when the first job fails, kill all running jobs.
+# upon unexpected termination, signal jobs before killing (signal, timeout)
+# execute all *.sh files in parallel
+parallel \
+        --line-buffer \
+        --halt now,fail=1 \
+        --termseq INT,200,TERM,100,KILL,25 \
+        '{}' ::: /runners/*.sh &
+
+child=$!
+wait "$child"
+echo "Exiting"
