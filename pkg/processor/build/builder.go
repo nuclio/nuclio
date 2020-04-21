6150507273adc6a9f9aba6e47dca6c86fce45c56
Liran BG <liranbg@users.noreply.github.com>
2020-04-22 00:02:19 +0300

    Fix appending latest (#1630)

--- pkg/processor/build/builder.go
+++ pkg/processor/build/builder.go
@@ -491,6 +491,11 @@ func (b *Builder) validateAndEnrichConfiguration() error {
 		b.processorImage.imageName = processorImageName
 	}
 
+	splitProcessorImageName := strings.Split(b.processorImage.imageName, ":")
+	if len(splitProcessorImageName) == 2 {
+		b.processorImage.imageTag = splitProcessorImageName[1]
+	}
+
 	// if tag isn't set - set latest
 	if b.processorImage.imageTag == "" {
 		b.processorImage.imageTag = "latest"
@@ -501,7 +506,7 @@ func (b *Builder) validateAndEnrichConfiguration() error {
 		"platform", b.options.PlatformName,
 		"dependantImagesRegistryURL", b.options.DependantImagesRegistryURL,
 		"outputImageFile", b.options.OutputImageFile,
-		"pi", b.processorImage)
+		"processorImage", b.processorImage)
 
 	return nil
 }
