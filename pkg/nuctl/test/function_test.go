b36386f48a6cc0a21fb522b0c658e2c381d89434
Liran BG <liranbg@users.noreply.github.com>
2020-05-12 12:41:41 +0300

    Make delete resource idempotent (#1662)

--- pkg/nuctl/test/function_test.go
+++ pkg/nuctl/test/function_test.go
@@ -581,6 +581,64 @@ func (suite *functionGetTestSuite) TestGet() {
 	suite.findPatternsInOutput(functionNames, nil)
 }
 
+type functionDeleteTestSuite struct {
+	Suite
+}
+
+func (suite *functionGetTestSuite) TestDelete() {
+	var err error
+
+	uniqueSuffix := xid.New().String()
+	imageName := "nuclio/deploy-test" + uniqueSuffix
+	functionName := "reverser" + uniqueSuffix
+
+	namedArgs := map[string]string{
+		"path":    path.Join(suite.GetFunctionsDir(), "common", "reverser", "golang"),
+		"image":   imageName,
+		"runtime": "golang",
+		"handler": "main:Reverse",
+	}
+
+	err = suite.ExecuteNuctl([]string{
+		"deploy",
+		functionName,
+		"--verbose",
+		"--no-pull",
+	}, namedArgs)
+	suite.Require().NoError(err)
+
+	// cleanup
+	defer suite.dockerClient.RemoveImage(imageName)
+
+	// try a few times to invoke, until it succeeds
+	err = suite.ExecuteNuctlAndWait([]string{"invoke", functionName},
+		map[string]string{
+			"method": "POST",
+			"body":   "-reverse this string+",
+			"via":    "external-ip",
+		},
+		false)
+	suite.Require().NoError(err)
+
+	// function removed
+	err = suite.ExecuteNuctl([]string{"delete", "fu", functionName}, nil)
+	suite.Require().NoError(err)
+
+	// ensure delete is idempotent
+	err = suite.ExecuteNuctl([]string{"delete", "fu", functionName}, nil)
+	suite.Require().NoError(err)
+
+	// try invoke, it should failed
+	err = suite.ExecuteNuctlAndWait([]string{"invoke", functionName},
+		map[string]string{
+			"method": "POST",
+			"body":   "-reverse this string+",
+			"via":    "external-ip",
+		},
+		true)
+	suite.Require().NoError(err, "Function was suppose to be deleted!")
+}
+
 func TestFunctionTestSuite(t *testing.T) {
 	if testing.Short() {
 		return
@@ -589,4 +647,5 @@ func TestFunctionTestSuite(t *testing.T) {
 	suite.Run(t, new(functionBuildTestSuite))
 	suite.Run(t, new(functionDeployTestSuite))
 	suite.Run(t, new(functionGetTestSuite))
+	suite.Run(t, new(functionDeleteTestSuite))
 }
