a7fd6f05a0a1c512612cda7d78bb3f230453c03c
Liran BG <liranbg@users.noreply.github.com>
2020-06-30 14:14:37 +0300

    Nuclio python wrapper termination fix (#1723)

--- pkg/processor/runtime/python/py/test_wrapper.py
+++ pkg/processor/runtime/python/py/test_wrapper.py
@@ -70,6 +70,27 @@ class TestSubmitEvents(unittest.TestCase):
         self._unix_stream_server.shutdown()
         self._unix_stream_server_thread.join()
 
+    def test_bad_function_code(self):
+        def raise_exception(ctx, event):
+            raise RuntimeError(error_message)
+
+        error_message = 'Im a bad entrypoint'
+        self._wait_for_socket_creation()
+        self._send_event(nuclio_sdk.Event(_id='1'))
+
+        self._wrapper._entrypoint = raise_exception
+        self._wrapper.serve_requests(num_requests=1)
+
+        # processor start, function log line, response body
+        self._wait_until_received_messages(3)
+
+        # extract the response
+        response = next(message['body']
+                        for message in self._unix_stream_server._messages
+                        if message['type'] == 'r')
+        response_body = response['body']
+        self.assertIn(error_message, response_body)
+
     def test_event_illegal_message_size(self):
         def _send_illegal_message_size():
             self._unix_stream_server._connection_socket.sendall(struct.pack(">I", 0))
@@ -80,7 +101,8 @@ class TestSubmitEvents(unittest.TestCase):
 
         self._wrapper._entrypoint = mock.MagicMock()
         self._wrapper._entrypoint.assert_not_called()
-        self._wrapper.serve_requests(num_requests=1)
+        with self.assertRaises(SystemExit):
+            self._wrapper.serve_requests(num_requests=1)
         t.join()
 
     def test_single_event(self):
