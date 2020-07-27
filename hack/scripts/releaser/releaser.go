45c806b55172e6b1286632f0b137f5af7705f979
Liran BG <liranbg@gmail.com>
2020-07-27 18:10:17 +0300

    with that

--- hack/scripts/releaser/releaser.go
+++ hack/scripts/releaser/releaser.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2017 The Nuclio Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package main
 
 import (
@@ -512,6 +528,15 @@ func run() error {
 	flag.StringVar(&release.helmChartsTargetVersion, "helm-charts-release-version", "", "Helm charts release target version")
 	flag.BoolVar(&release.publishHelmCharts, "publish-helm-charts", true, "Whether to publish helm charts")
 	flag.Parse()
+
+	release.logger.InfoWith("Running release",
+		"targetVersion", release.targetVersion,
+		"repositoryOwnerName", release.repositoryOwnerName,
+		"repositoryScheme", release.repositoryScheme,
+		"developmentBranch", release.developmentBranch,
+		"releaseBranch", release.releaseBranch,
+		"helmChartsTargetVersion", release.helmChartsTargetVersion,
+		"publishHelmCharts", release.publishHelmCharts)
 	return release.Run()
 }
 
