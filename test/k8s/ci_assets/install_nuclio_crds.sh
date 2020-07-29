77d7f4df79f497c8f662ac39508e4518e65d5ec4
LiranBG <liranbg@gmail.com>
2020-07-29 11:31:38 +0300

    add to ci

---
+++ test/k8s/ci_assets/install_nuclio_crds.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "Installing nuclio CRDs only"
+helm install \
+    --set controller.enabled=false \
+    --set dashboard.enabled=false \
+    --set autoscaler.enabled=false \
+    --set dlx.enabled=false \
+    --set rbac.create=false \
+    --set crd.create=true \
+    --debug \
+    --wait \
+    --namespace ${NAMESPACE:-nuclio} \
+    nuclio hack/k8s/helm/nuclio
