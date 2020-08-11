61444f14bf87b31759b5ce46d50e012aed6cb23b
Liran BG <liranbg@users.noreply.github.com>
2020-08-11 23:20:29 +0300

    Allow processor config to be read by other users (#1785)

--- pkg/platform/local/platform.go
+++ pkg/platform/local/platform.go
@@ -659,6 +659,11 @@ func (p *Platform) createProcessorConfig(createFunctionOptions *platform.CreateF
 		return "", errors.Wrap(err, "Failed to write processor config")
 	}
 
+	// make it readable by other users, in case user use different USER directive on function image
+	if err := os.Chmod(processorConfigFile.Name(), 0644); err != nil {
+		return "", errors.Wrap(err, "Failed to change processor config file permission")
+	}
+
 	p.Logger.DebugWith("Wrote processor configuration", "path", processorConfigFile.Name())
 
 	// read the file once for logging
