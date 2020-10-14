3491053d5a0bccc3be8fe21791629344c0489937
sahare92 <sahare92@gmail.com>
2020-10-14 16:09:55 +0300

    ignore failure of api gateways enrichment only on api gateways crd not found

--- pkg/platform/kube/platform.go
+++ pkg/platform/kube/platform.go
@@ -342,7 +342,13 @@ func (p *Platform) GetFunctions(getFunctionsOptions *platform.GetFunctionsOption
 	p.EnrichFunctionsWithDeployLogStream(functions)
 
 	if err = p.enrichFunctionsWithAPIGateways(functions, getFunctionsOptions.Namespace); err != nil {
-		p.Logger.WarnWith("Failed to enrich functions with api gateways. Ignoring",
+
+		// relevant when upgrading nuclio from a version that didn't have api-gateways to one that has
+		if !strings.Contains(err.Error(), "the server could not find the requested resource (get nuclioapigateways.nuclio.io)") {
+			return nil, errors.Wrap(err, "Failed to enrich functions with api gateways")
+		}
+
+		p.Logger.WarnWith("API Gateways are not supported on this system. Ignoring their enrichment failure",
 			"err", err)
 	}
 
