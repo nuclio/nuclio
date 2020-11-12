30e527e32b0b1aa17999f29b71ae1f0d4f532d0b
Liran BG <liranbg@gmail.com>
2020-11-11 23:20:59 -0500

    lint

--- pkg/processor/trigger/http/test/suite/http_test.go
+++ pkg/processor/trigger/http/test/suite/http_test.go
@@ -71,10 +71,10 @@ func (suite *HTTPTestSuite) TestCORS() {
 				},
 				ExpectedResponseStatusCode: &validPreflightResponseStatusCode,
 				ExpectedResponseHeadersValues: map[string][]string{
-					"Access-Control-Allow-Methods":  {allowMethods},
-					"Access-Control-Allow-Headers":  {allowHeaders},
-					"Access-Control-Allow-Origin":   {origin},
-					"Access-Control-Max-Age":        {strconv.Itoa(preflightMaxAgeSeconds)},
+					"Access-Control-Allow-Methods": {allowMethods},
+					"Access-Control-Allow-Headers": {allowHeaders},
+					"Access-Control-Allow-Origin":  {origin},
+					"Access-Control-Max-Age":       {strconv.Itoa(preflightMaxAgeSeconds)},
 				},
 			},
 			{
