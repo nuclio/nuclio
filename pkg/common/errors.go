4a9e8584d9a37a3859f889cb054ec10130c4bf79
sahare92 <sahare92@gmail.com>
2020-11-23 13:44:16 +0200

    ...

--- pkg/common/errors.go
+++ pkg/common/errors.go
@@ -18,7 +18,7 @@ func ResolveErrorStatusCodeOrDefault(err error, defaultStatusCode int) int {
 	}
 
 	// resolve from root cause
-	switch rootCauseTypedError := errors.Cause(err).(type) {
+	switch rootCauseTypedError := errors.RootCause(err).(type) {
 	case nuclio.ErrorWithStatusCode:
 		return rootCauseTypedError.StatusCode()
 	case *nuclio.ErrorWithStatusCode:
