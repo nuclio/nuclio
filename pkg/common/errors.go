c984caf5d4abc199ba74a49571b3f4347a615f59
sahare92 <sahare92@gmail.com>
2020-11-29 12:27:58 +0200

    ...

--- pkg/common/errors.go
+++ pkg/common/errors.go
@@ -22,10 +22,11 @@ func ResolveErrorStatusCodeOrDefault(err error, defaultStatusCode int) int {
 			return typedError.StatusCode()
 		}
 
+		// in case the previous level didn't have a status code - get the next cause in the error stack
 		cause := errors.Cause(currentErr)
 
 		// if there's no cause, we're done
-		// if the cause is not comparabile, it's not an Error and we're done
+		// if the cause is not comparable, it's not an Error and we're done
 		// if the cause == the error, we're done since that's what Cause() returns
 		if cause == nil || !reflect.TypeOf(cause).Comparable() || cause == currentErr {
 			break
