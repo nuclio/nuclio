1103224aaf13fb2fac3520be76045e4e5b791310
Liran BG <liranbg@gmail.com>
2021-02-28 20:11:16 -0500

    disable resync interval on tests

--- pkg/platform/kube/test/suite.go
+++ pkg/platform/kube/test/suite.go
@@ -543,7 +543,7 @@ func (suite *KubeTestSuite) createController() *controller.Controller {
 		nuclioClientSet,
 		functionresClient,
 		apigatewayresClient,
-		time.Second*5,  // resync interval
+		0,  // disable resync interval
 		time.Second*5,  // monitor interval
 		time.Second*30, // cronjob stale duration
 		suite.PlatformConfiguration,
