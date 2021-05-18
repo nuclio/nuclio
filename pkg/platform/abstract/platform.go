c7015c2cd549b3954ab3414caf08641e784d0d81
sahare92 <sahare92@gmail.com>
2021-05-18 14:01:52 +0300

    fix ensure default project existence

--- pkg/platform/abstract/platform.go
+++ pkg/platform/abstract/platform.go
@@ -42,6 +42,7 @@ import (
 	"github.com/nuclio/nuclio-sdk-go"
 	"golang.org/x/sync/errgroup"
 	"k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/util/validation"
 )
 
@@ -783,6 +784,12 @@ func (ap *Platform) EnsureDefaultProjectExistence() error {
 		if err := ap.platform.CreateProject(&platform.CreateProjectOptions{
 			ProjectConfig: newProject.GetConfig(),
 		}); err != nil {
+
+			// if project already exists, return
+			if apierrors.IsAlreadyExists(err) {
+				return nil
+			}
+
 			return errors.Wrap(err, "Failed to create default project")
 		}
 
