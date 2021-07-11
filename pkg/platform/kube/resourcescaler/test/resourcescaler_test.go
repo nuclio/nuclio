1c2ed0d515a128f37933298f0c8a0e62dd02b005
Liran BG <liranbg@gmail.com>
2021-07-11 10:01:56 +0300

    cont

--- pkg/platform/kube/resourcescaler/test/resourcescaler_test.go
+++ pkg/platform/kube/resourcescaler/test/resourcescaler_test.go
@@ -21,15 +21,17 @@ package test
 
 import (
 	"context"
+
 	"fmt"
+	"net/http"
 	"testing"
 	"time"
 
+	"github.com/nuclio/nuclio/pkg/common"
 	"github.com/nuclio/nuclio/pkg/functionconfig"
 	"github.com/nuclio/nuclio/pkg/platform"
 	"github.com/nuclio/nuclio/pkg/platform/kube/resourcescaler"
 	"github.com/nuclio/nuclio/pkg/platform/kube/test"
-	"github.com/nuclio/nuclio/pkg/platformconfig"
 
 	"github.com/stretchr/testify/suite"
 	"github.com/v3io/scaler/pkg/autoscaler"
@@ -38,7 +40,8 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
-	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
+	k8stesting "k8s.io/client-go/testing"
 	"k8s.io/metrics/pkg/apis/custom_metrics/v1beta2"
 	"k8s.io/metrics/pkg/client/custom_metrics/fake"
 )
@@ -54,15 +57,6 @@ type ResourceScalerTestSuite struct {
 func (suite *ResourceScalerTestSuite) SetupSuite() {
 	var err error
 
-	suite.PlatformConfiguration = &platformconfig.Config{}
-	suite.PlatformConfiguration.ScaleToZero.ScaleResources = []functionconfig.ScaleResource{
-		{
-			MetricName: "something",
-			WindowSize: "1m",
-			Threshold:  5,
-		},
-	}
-
 	suite.KubeTestSuite.SetupSuite()
 	resourceScaler, err := resourcescaler.New(suite.Logger,
 		suite.Namespace,
@@ -73,6 +67,10 @@ func (suite *ResourceScalerTestSuite) SetupSuite() {
 
 	resourceScalerConfig, err := resourceScaler.GetConfig()
 	suite.Require().NoError(err)
+
+	resourceScalerConfig.AutoScalerOptions.ScaleInterval = scalertypes.Duration{
+		Duration: 5 * time.Second,
+	}
 
 	suite.dlx, err = dlx.NewDLX(suite.Logger, resourceScaler, resourceScalerConfig.DLXOptions)
 	suite.Require().NoError(err)
@@ -116,101 +114,91 @@ func (suite *ResourceScalerTestSuite) TestSanity() {
 	createFunctionOptions.FunctionConfig.Spec.MinReplicas = &zero
 	createFunctionOptions.FunctionConfig.Spec.MaxReplicas = &one
 	createFunctionOptions.FunctionConfig.Spec.ScaleToZero = &functionconfig.ScaleToZeroSpec{
-		ScaleResources: suite.PlatformConfiguration.ScaleToZero.ScaleResources,
+		ScaleResources: []functionconfig.ScaleResource{
+			{
+				MetricName: "something",
+				Threshold:  1,
+				WindowSize: "250ms",
+			},
+		},
 	}
-
-	// when autoscaler requests metric from the custom metric client
-	// ensure that its response is mocked
-	suite.mockMetricClient(functionName, resource.MustParse("0"))
 
 	suite.DeployFunction(createFunctionOptions, func(deployResult *platform.CreateFunctionResult) bool {
-		suite.scaleFunctionToZero(functionName)
-		suite.scaleFunctionFromZero(functionName)
+
+		// add metrics data
+		// make metric client return value that is small enough (e.g.: 0)
+		// to ensure scaler will scale the function to zero
+		suite.metricClient.AddReactor("get", "nucliofunctions.nuclio.io", func(action k8stesting.Action) (
+			handled bool, ret runtime.Object, err error) {
+			return true, &v1beta2.MetricValueList{
+				Items: []v1beta2.MetricValue{
+					{
+						DescribedObject: v1.ObjectReference{
+							Name:      functionName,
+							Namespace: suite.Namespace,
+						},
+						Value: resource.MustParse("0"),
+					},
+				},
+			}, nil
+		})
+
+		// wait for the function to scale to zero
+		suite.WaitForFunctionState(&platform.GetFunctionsOptions{
+			Namespace: suite.Namespace,
+			Name:      functionName,
+		},
+			functionconfig.FunctionStateScaledToZero,
+			30*time.Second)
+
+		// ensure function deployment replicas is zero
+		suite.WaitForFunctionDeployment(functionName, 15*time.Second, func(deployment *appsv1.Deployment) bool {
+			return deployment.Status.Replicas == 0
+		})
+
+		// function has scaled to zero, remove the reactor we added to send "fake" metric values
+		suite.metricClient.ReactionChain = suite.metricClient.ReactionChain[:len(suite.metricClient.ReactionChain)-1]
+
+		// try invoke function without the target header
+		// expect DLX to fail on 400
+		_, _, _ = common.SendHTTPRequest(http.MethodGet,
+			fmt.Sprintf("http://%s:8080", suite.GetTestHost()),
+			[]byte{},
+			map[string]string{},
+			nil,
+			http.StatusBadRequest,
+			true,
+			30*time.Second)
+
+		// add target header, expect it to wake up the function
+		// for this specific test case, the response status code is 502
+		// reason dlx tries to reverse-proxy the request to the function by its service
+		// and since the dlx component is running as a process (and not as a POD)
+		// it fails to resolve the internal (kubernetes) function host
+		// TODO: make DLX work in "test" mode, where it invoke the function from within the k8s cluster
+		//       see suite.KubectlInvokeFunctionViaCurl(functionName, "http://function-service-endpoint:8080")
+		responseBody, _, err := common.SendHTTPRequest(http.MethodGet,
+			fmt.Sprintf("http://%s:8080", suite.GetTestHost()),
+			[]byte{},
+			map[string]string{
+				"X-Nuclio-Target": functionName,
+			},
+			nil,
+			0,
+			true,
+			30*time.Second)
+		suite.Require().NoError(err)
+		suite.Require().Equal([]byte{}, responseBody)
+
+		// function has woken up
+		suite.WaitForFunctionState(&platform.GetFunctionsOptions{
+			Namespace: suite.Namespace,
+			Name:      functionName,
+		},
+			functionconfig.FunctionStateReady,
+			30*time.Second)
 		return true
 	})
-}
-
-func (suite *ResourceScalerTestSuite) scaleFunctionToZero(functionName string) {
-	suite.Logger.InfoWith("Scaling function to zero", "functionName", functionName)
-
-	// get function resources
-	resources, err := suite.resourceScaler.GetResources()
-	suite.Require().NoError(err)
-
-	// scale function to zero
-	err = suite.resourceScaler.SetScale(resources, 0)
-	suite.Require().NoError(err)
-
-	// wait for function to scale
-	suite.WaitForFunctionState(&platform.GetFunctionsOptions{
-		Namespace: suite.Namespace,
-		Name:      functionName,
-	},
-		functionconfig.FunctionStateScaledToZero,
-		30*time.Second)
-
-	// ensure function deployment replicas is zero
-	suite.WaitForFunctionDeployment(functionName, 15*time.Second, func(deployment *appsv1.Deployment) bool {
-		return deployment.Status.Replicas == 0
-	})
-}
-
-func (suite *ResourceScalerTestSuite) scaleFunctionFromZero(functionName string) {
-	suite.Logger.InfoWith("Scaling function from zero", "functionName", functionName)
-
-	// scale function from zero
-	err := suite.resourceScaler.SetScale([]scalertypes.Resource{
-		{
-			Name:      functionName,
-			Namespace: suite.Namespace,
-		},
-	}, 1)
-	suite.Require().NoError(err)
-
-	// wait for function to scale
-	suite.WaitForFunctionState(&platform.GetFunctionsOptions{
-		Namespace: suite.Namespace,
-		Name:      functionName,
-	},
-		functionconfig.FunctionStateReady,
-		30*time.Second)
-
-	// ensure function deployment replicas is zero
-	suite.WaitForFunctionDeployment(functionName, 15*time.Second, func(deployment *appsv1.Deployment) bool {
-		return deployment.Status.Replicas == 1
-	})
-}
-
-func (suite *ResourceScalerTestSuite) mockMetricClient(functionName string, value resource.Quantity) {
-	config, _ := suite.resourceScaler.GetConfig()
-	windowSizeDuration, _ := time.ParseDuration(suite.PlatformConfiguration.ScaleToZero.ScaleResources[0].WindowSize)
-
-	functionResource := scalertypes.Resource{}
-	functionResource.ScaleResources = []scalertypes.ScaleResource{
-		{
-			MetricName: suite.PlatformConfiguration.ScaleToZero.ScaleResources[0].MetricName,
-			WindowSize: scalertypes.Duration{
-				Duration: windowSizeDuration,
-			},
-			Threshold: suite.PlatformConfiguration.ScaleToZero.ScaleResources[0].Threshold,
-		},
-	}
-	action := fake.NewGetForAction(config.AutoScalerOptions.GroupKind,
-		suite.Namespace,
-		"*",
-		functionResource.ScaleResources[0].GetKubernetesMetricName(), labels.Everything())
-	_, err := suite.metricClient.Invokes(action, &v1beta2.MetricValueList{
-		Items: []v1beta2.MetricValue{
-			{
-				DescribedObject: v1.ObjectReference{
-					Name:      functionName,
-					Namespace: suite.Namespace,
-				},
-				Value: value,
-			},
-		},
-	})
-	suite.Require().NoError(err)
 }
 
 func TestControllerTestSuite(t *testing.T) {
