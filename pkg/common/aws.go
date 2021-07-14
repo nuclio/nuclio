723f854df7fee189ed522bde8dab432f5a18bdcf
Liran BG <liranbg@gmail.com>
2021-07-14 13:14:26 +0300

    s3 mock

--- pkg/common/aws.go
+++ pkg/common/aws.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"io/ioutil"
 	"os"
 	"path"
 	"path/filepath"
@@ -11,6 +12,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/nuclio/errors"
+	"github.com/stretchr/testify/mock"
 )
 
 type S3Client interface {
@@ -76,4 +78,27 @@ func (asc *AbstractS3Client) download(file *os.File, sess *session.Session, buck
 		return errors.Wrap(err, "Failed to download file from s3")
 	}
 	return nil
+}
+
+type MockS3Client struct {
+	mock.Mock
+	FilePath string
+}
+
+func (msc *MockS3Client) Download(file *os.File, bucket, itemKey, region, accessKeyID, secretAccessKey, sessionToken string) error {
+	functionArchiveFileBytes, _ := ioutil.ReadFile(msc.FilePath)
+
+	_ = ioutil.WriteFile(file.Name(), functionArchiveFileBytes, os.FileMode(os.O_RDWR))
+
+	args := msc.Called(file, bucket, itemKey, region, accessKeyID, secretAccessKey, sessionToken)
+	return args.Error(0)
+}
+
+func (msc *MockS3Client) DownloadWithinEC2Instance(file *os.File, bucket, itemKey string) error {
+	functionArchiveFileBytes, _ := ioutil.ReadFile(msc.FilePath)
+
+	_ = ioutil.WriteFile(file.Name(), functionArchiveFileBytes, os.FileMode(os.O_RDWR))
+
+	args := msc.Called(file, bucket, itemKey)
+	return args.Error(0)
 }
