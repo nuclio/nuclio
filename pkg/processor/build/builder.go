d4945356f1077f0c84a4bcff4b1ec4514807c5db
Liran BG <liranbg@gmail.com>
2021-09-19 08:50:03 +0300

    err leak

--- pkg/processor/build/builder.go
+++ pkg/processor/build/builder.go
@@ -215,7 +215,7 @@ func (b *Builder) Build(options *platform.CreateFunctionBuildOptions) (*platform
 
 	// prepare configuration from both configuration files and things builder infers
 	if !configurationRead {
-		if _, err = b.readConfiguration(); err != nil {
+		if _, err := b.readConfiguration(); err != nil {
 			return nil, errors.Wrap(err, "Failed to read configuration")
 		}
 	}
@@ -228,7 +228,7 @@ func (b *Builder) Build(options *platform.CreateFunctionBuildOptions) (*platform
 
 	// once we're done reading our configuration, we may still have to fill in the blanks
 	// because the user isn't obligated to always pass all the configuration
-	if err = b.validateAndEnrichConfiguration(); err != nil {
+	if err := b.validateAndEnrichConfiguration(); err != nil {
 		return nil, errors.Wrap(err, "Failed to enrich configuration")
 	}
 
@@ -244,13 +244,13 @@ func (b *Builder) Build(options *platform.CreateFunctionBuildOptions) (*platform
 
 	// if a callback is registered, call back
 	if b.options.OnAfterConfigUpdate != nil {
-		if err = b.options.OnAfterConfigUpdate(&enrichedConfiguration); err != nil {
+		if err := b.options.OnAfterConfigUpdate(&enrichedConfiguration); err != nil {
 			return nil, errors.Wrap(err, "OnAfterConfigUpdate returned error")
 		}
 	}
 
 	// prepare a staging directory
-	if err = b.prepareStagingDir(); err != nil {
+	if err := b.prepareStagingDir(); err != nil {
 		return nil, errors.Wrap(err, "Failed to prepare staging dir")
 	}
 
@@ -622,8 +622,7 @@ func (b *Builder) writeFunctionSourceCodeToTempFile(functionSourceCode string) (
 	sourceFilePath := path.Join(tempDir, moduleFileName)
 
 	b.logger.DebugWith("Writing function source code to temporary file", "functionPath", sourceFilePath)
-	err = ioutil.WriteFile(sourceFilePath, decodedFunctionSourceCode, os.FileMode(0644))
-	if err != nil {
+	if err := ioutil.WriteFile(sourceFilePath, decodedFunctionSourceCode, os.FileMode(0644)); err != nil {
 		return "", errors.Wrapf(err, "Failed to write given source code to file %s", sourceFilePath)
 	}
 
@@ -947,7 +946,7 @@ func (b *Builder) prepareStagingDir() error {
 	}
 
 	// first, tell the specific runtime to do its thing
-	if err := b.runtime.OnAfterStagingDirCreated(b.stagingDir); err != nil {
+	if err := b.runtime.OnAfterStagingDirCreated(b.platform.GetConfig().Runtime, b.stagingDir); err != nil {
 		return errors.Wrap(err, "Failed to prepare staging dir")
 	}
 
@@ -1374,7 +1373,7 @@ func (b *Builder) getDockerFileBuildArgs() map[string]string {
 			buildArgs[key] = value
 		} else {
 
-			// value is empty, remote this arg
+			// value is empty, remove this arg
 			delete(buildArgs, key)
 		}
 	}
