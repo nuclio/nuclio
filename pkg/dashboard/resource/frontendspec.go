6215f7e9bc9eadb81c5c19f2f028b9718be58414
TomerShor <90552140+TomerShor@users.noreply.github.com>
2022-05-03 11:26:37 +0300

    Add default function pod resources to frontend spec (#2560)

--- pkg/dashboard/resource/frontendspec.go
+++ pkg/dashboard/resource/frontendspec.go
@@ -82,6 +82,7 @@ func (fsr *frontendSpecResource) getFrontendSpec(request *http.Request) (*restfu
 	defaultFunctionConfig := fsr.getDefaultFunctionConfig()
 	defaultHTTPIngressHostTemplate := fsr.getDefaultHTTPIngressHostTemplate()
 	validFunctionPriorityClassNames := fsr.resolveValidFunctionPriorityClassNames()
+	defaultFunctionPodResources := fsr.resolveDefaultFunctionPodResources()
 
 	frontendSpec := map[string]restful.Attributes{
 		"frontendSpec": { // frontendSpec is the ID of this singleton resource
@@ -94,6 +95,7 @@ func (fsr *frontendSpecResource) getFrontendSpec(request *http.Request) (*restfu
 			"platformKind":                    platformKind,
 			"allowedAuthenticationModes":      allowedAuthenticationModes,
 			"validFunctionPriorityClassNames": validFunctionPriorityClassNames,
+			"defaultFunctionPodResources":     defaultFunctionPodResources,
 		},
 	}
 
@@ -212,6 +214,14 @@ func (fsr *frontendSpecResource) resolveDefaultFunctionPriorityClassName() strin
 		defaultFunctionPriorityClassName = dashboardServer.GetPlatformConfiguration().Kube.DefaultFunctionPriorityClassName
 	}
 	return defaultFunctionPriorityClassName
+}
+
+func (fsr *frontendSpecResource) resolveDefaultFunctionPodResources() platformconfig.PodResourceRequirements {
+	var defaultFunctionPodResources platformconfig.PodResourceRequirements
+	if dashboardServer, ok := fsr.resource.GetServer().(*dashboard.Server); ok {
+		defaultFunctionPodResources = dashboardServer.GetPlatformConfiguration().Kube.DefaultFunctionPodResources
+	}
+	return defaultFunctionPodResources
 }
 
 func (fsr *frontendSpecResource) resolveValidFunctionPriorityClassNames() []string {
