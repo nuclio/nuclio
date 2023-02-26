386900e7fd7fe6b782ea319921a86a466ccdfd5e
TomerShor <90552140+TomerShor@users.noreply.github.com>
2023-02-26 11:07:54 +0200

    [FunctionConfig] Fix checking replica values instead of pointers (#2843)

--- pkg/platform/abstract/platform.go
+++ pkg/platform/abstract/platform.go
@@ -1547,7 +1547,7 @@ func (ap *Platform) validateTriggers(functionConfig *functionconfig.Config) erro
 		if lo.Contains[string]([]string{"v3io-stream", "v3ioStream"}, triggerInstance.Kind) {
 
 			// V3IO stream trigger does not support autoscaling, so min and max replicas must be equal
-			if functionConfig.Spec.MinReplicas != functionConfig.Spec.MaxReplicas {
+			if *functionConfig.Spec.MinReplicas != *functionConfig.Spec.MaxReplicas {
 				return nuclio.NewErrBadRequest("V3IO Stream trigger does not support autoscaling")
 			}
 		}
