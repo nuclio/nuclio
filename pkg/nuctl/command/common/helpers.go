c2d1aac4c6cf3d6fdbbbb3c19f3ad60f844b7184
TomerShor <90552140+TomerShor@users.noreply.github.com>
2023-04-16 12:01:10 +0300

    [Nuctl] Support function redeployment using the `beta` api client (#2882)

--- pkg/nuctl/command/common/helpers.go
+++ pkg/nuctl/command/common/helpers.go
@@ -21,6 +21,8 @@ import (
 	"io"
 	"os"
 
+	"github.com/nuclio/nuclio/pkg/functionconfig"
+
 	"github.com/nuclio/errors"
 	"sigs.k8s.io/yaml"
 )
@@ -77,4 +79,22 @@ func GetUnmarshalFunc(bytes []byte) (func(data []byte, v interface{}) error, err
 	}
 
 	return nil, errors.New("Input is neither json nor yaml")
+}
+
+// ConvertMapToFunctionConfig converts a map to a function config
+func ConvertMapToFunctionConfig(functionMap map[string]interface{}) (functionconfig.Config, error) {
+	var functionConfig functionconfig.Config
+
+	// convert to json
+	functionConfigJSON, err := json.Marshal(functionMap)
+	if err != nil {
+		return functionConfig, errors.Wrap(err, "Failed to marshal function config")
+	}
+
+	// convert to function config
+	if err := json.Unmarshal(functionConfigJSON, &functionConfig); err != nil {
+		return functionConfig, errors.Wrap(err, "Failed to unmarshal function config")
+	}
+
+	return functionConfig, nil
 }
