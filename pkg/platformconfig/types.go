0ee227b0669b7ae42c6ef66e7befc90344ce2be9
TomerShor <tomer.shor@gmail.com>
2023-04-16 10:51:43 +0300

    add annotations to masked fields

--- pkg/platformconfig/types.go
+++ pkg/platformconfig/types.go
@@ -328,6 +328,13 @@ func (sfc *SensitiveFieldsConfig) GetDefaultSensitiveFields() []string {
 		"^/spec/triggers/.+/attributes/accesscertificate$",
 		"^/spec/triggers/.+/attributes/sasl/password$",
 		"^/spec/triggers/.+/attributes/sasl/oauth/clientsecret$",
+		// - kafka annotations
+		"^/metadata/annotations/nuclio.io/kafka-ca-cert$",
+		"^/metadata/annotations/nuclio.io/kafka-access-key$",
+		"^/metadata/annotations/nuclio.io/kafka-access-cert$",
+		"^/metadata/annotations/nuclio.io/kafka-sasl-password$",
+		"^/metadata/annotations/nuclio.io/kafka-sasl-oauth-client-secret$",
+		"^/metadata/annotations/nuclio.io/kafka-sasl-oauth-token-url$",
 	}
 }
 
