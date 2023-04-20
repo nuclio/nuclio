bf9ee63283992a783db8a6ee453d623f896dcc8a
TomerShor <tomer.shor@gmail.com>
2023-04-20 17:04:05 +0300

    [Assets] Install nginx ingress controller using helm

--- test/k8s/ci_assets/install_nginx_ingress_controller.sh
+++ test/k8s/ci_assets/install_nginx_ingress_controller.sh
@@ -14,4 +14,6 @@
 # limitations under the License.
 #
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.34.1/deploy/static/provider/cloud/deploy.yaml
+helm upgrade --install ingress-nginx ingress-nginx \
+  --repo https://kubernetes.github.io/ingress-nginx \
+  --namespace ingress-nginx --create-namespace
