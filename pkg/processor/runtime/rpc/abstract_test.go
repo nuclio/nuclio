56a2c46d621ad712b1cfc66fa891f46aa15df22a
Katerina Molchanova <35141662+rokatyy@users.noreply.github.com>
2023-08-17 09:22:40 +0300

    [Helm] Fix creation of ClusterRoleBinding for namespace access mode (#2986)

--- pkg/processor/runtime/rpc/abstract_test.go
+++ pkg/processor/runtime/rpc/abstract_test.go
@@ -64,9 +64,6 @@ func newTestRuntime(parentLogger logger.Logger, configuration *runtime.Configura
 
 	newTestRuntime.AbstractRuntime.ControlMessageBroker = NewRpcControlMessageBroker(nil, parentLogger, nil)
 
-	// set the runtime's isDrained to true, so it won't send a signal to the wrapper process
-	newTestRuntime.isDrained = true
-
 	return newTestRuntime, nil
 }
 
@@ -208,6 +205,9 @@ func (suite *RuntimeSuite) TestReadControlMessage() {
 }
 
 func (suite *RuntimeSuite) TearDownTest() {
+	// set the runtime's isDrained to true, so it won't send a signal to the wrapper process
+	suite.testRuntimeInstance.isDrained = true
+
 	if suite.testRuntimeInstance != nil && suite.testRuntimeInstance.wrapperProcess != nil {
 		suite.testRuntimeInstance.Stop() // nolint: errcheck
 	}
