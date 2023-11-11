5186f8b55b77ebf4eadacae07c4da55ab532e204
KonsumGandalf <david_schmidt_privat@outlook.com>
2023-11-11 14:28:04 +0100

    refactor(profaastinate-david): testing application
    Remove unused key sets.

--- profaastinate/prototyping/david/cmd/test-application/main.go
+++ profaastinate/prototyping/david/cmd/test-application/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/konsumgandalf/mpga-protoype-david/configs"
 	"github.com/konsumgandalf/mpga-protoype-david/pkg/redis/prototyping"
 )
@@ -11,17 +10,6 @@ func main() {
 	ctx := context.Background()
 
 	rdb := configs.SetupRedis(ctx)
-
-	err := rdb.Set(ctx, "key", "value2", 0).Err()
-	if err != nil {
-		panic(err)
-	}
-
-	val, err := rdb.Get(ctx, "key").Result()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("key", val)
 
 	prototyping.TestRedis(ctx, rdb)
 }
