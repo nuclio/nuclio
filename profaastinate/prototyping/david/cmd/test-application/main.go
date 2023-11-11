18333f3ab15b19b24ab9375a5bcfa8f7e05a5cdf
KonsumGandalf <david_schmidt_privat@outlook.com>
2023-11-11 14:27:30 +0100

    feat(profaastinate-david): testing application
    Add test application with prototyping function

--- profaastinate/prototyping/david/cmd/test-application/main.go
+++ profaastinate/prototyping/david/cmd/test-application/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/konsumgandalf/mpga-protoype-david/configs"
 	"github.com/konsumgandalf/mpga-protoype-david/pkg/redis/prototyping"
 )
@@ -10,6 +11,17 @@ func main() {
 	ctx := context.Background()
 
 	rdb := configs.SetupRedis(ctx)
+
+	err := rdb.Set(ctx, "key", "value2", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := rdb.Get(ctx, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
 
 	prototyping.TestRedis(ctx, rdb)
 }
