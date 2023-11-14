de22db4eb4cdce6b14220002ed9ba87042d66020
rokatyy <ekaterina_molchanova@mckinsey.com>
2023-11-14 16:26:09 +0000

    lint

--- pkg/platform/kube/platform.go
+++ pkg/platform/kube/platform.go
@@ -21,7 +21,6 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"k8s.io/apimachinery/pkg/labels"
 	"net/url"
 	"strings"
 	"sync"
@@ -51,6 +50,7 @@ import (
 	networkingv1 "k8s.io/api/networking/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/cache"
 )
 
