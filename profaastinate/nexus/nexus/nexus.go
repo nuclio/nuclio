0acd595b79041b444e235a4e99ceac7a3888981e
Jonas Heisterberg <jonas@mennicke.com>
2023-12-14 20:53:14 +0100

    move mutex to queue & adjust tests

--- profaastinate/nexus/nexus/nexus.go
+++ profaastinate/nexus/nexus/nexus.go
@@ -3,31 +3,22 @@ package nexus
 import (
 	common "nexus/common/models/structs"
 	queue "nexus/common/queue"
-	"sync"
 )
 
 type Nexus struct {
 	Queue *queue.NexusQueue
-	Mu    *sync.RWMutex
 }
 
 func Init() *Nexus {
 	return &Nexus{
 		Queue: queue.Init(),
-		Mu:    &sync.RWMutex{},
 	}
 }
 
 func (n *Nexus) Push(elem *common.NexusItem) {
-	n.Mu.Lock()
-	defer n.Mu.Unlock()
-
 	n.Queue.Push(elem)
 }
 
 func (n *Nexus) Pop() interface{} {
-	n.Mu.Lock()
-	defer n.Mu.Unlock()
-
 	return n.Queue.Pop()
 }
