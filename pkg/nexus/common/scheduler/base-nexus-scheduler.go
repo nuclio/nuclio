5763b6561cf136e201f3a288f7c12a510f814f49
konsumgandalf <david_schmidt_privat@outlook.com>
2024-01-04 18:17:06 +0100

    fix(pkg-nexus): fix merge issues

--- pkg/nexus/common/scheduler/base-nexus-scheduler.go
+++ pkg/nexus/common/scheduler/base-nexus-scheduler.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"net/url"
 	"time"
--- pkg/nexus/common/utils/helper-functions.go
+++ pkg/nexus/common/utils/helper-functions.go
@@ -1,8 +1,12 @@
 package utils
 
 import (
+	"fmt"
 	"github.com/go-ping/ping"
+	"github.com/nuclio/nuclio/pkg/common/headers"
 	"github.com/nuclio/nuclio/pkg/nexus/common/models"
+	"net/http"
+	"net/url"
 )
 
 func GetEnvironmentHost() (host string) {
@@ -12,5 +16,21 @@ func GetEnvironmentHost() (host string) {
 	} else {
 		host = models.DARWIN_HOST
 	}
+	return
+}
+
+func TransformRequestToClientRequest(nexusItemRequest *http.Request) (newRequest *http.Request) {
+	if nexusItemRequest.Header.Get(headers.ProcessDeadline) != "" {
+		nexusItemRequest.Header.Del(headers.ProcessDeadline)
+	}
+
+	var requestUrl url.URL
+	requestUrl.Scheme = nexusItemRequest.URL.Scheme
+	requestUrl.Path = nexusItemRequest.URL.Path
+	// Needs to be set to the port of the environment the default port is 8080
+	requestUrl.Host = fmt.Sprintf("%s:%s", GetEnvironmentHost(), models.PORT)
+
+	newRequest, _ = http.NewRequest(nexusItemRequest.Method, requestUrl.String(), nexusItemRequest.Body)
+	newRequest.Header = nexusItemRequest.Header
 	return
 }
