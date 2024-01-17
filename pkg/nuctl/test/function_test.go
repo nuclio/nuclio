a2493b8926e4003b5ac4027a8aa395a42a8a3004
rokatyy <ekaterina_molchanova@mckinsey.com>
2024-01-17 15:33:49 +0000

    test

--- pkg/nuctl/test/function_test.go
+++ pkg/nuctl/test/function_test.go
@@ -1605,7 +1605,7 @@ func (suite *functionExportImportTestSuite) TestImportWithReport() {
 
 	projectReport, _ := projectsReport.GetReport(projectName)
 	suite.Require().NotNil(projectsReport)
-	suite.Require().Contains(projectReport.Failed.FailReason, "Import failed for some of the functions. Project: `test-project`")
+	suite.Require().Contains(projectReport.Failed.FailReason, "Import failed for some of the functions in project `test-project`.")
 
 	suite.Require().Equal(2, len(projectReport.FunctionReports.Success))
 	suite.Require().Contains(projectReport.FunctionReports.Success, "correct-test-function")
