69a522c30c255a78071ef24e3bc0545223113baa
TomerShor <tomer.shor@gmail.com>
2024-01-29 13:43:16 +0200

    revert test commenting

--- pkg/nuctl/test/function_test.go
+++ pkg/nuctl/test/function_test.go
@@ -1580,31 +1580,23 @@ func (suite *functionExportImportTestSuite) TestAutofixWhenImportFunction() {
 	suite.Require().NotNil(err)
 }
 
-/*
-// TODO: fix this test
 func (suite *functionExportImportTestSuite) TestImportWithReport() {
 	functionConfigPath := path.Join(suite.GetImportsDir(), "project_with_wrong_conf.yaml")
 	projectName := "test-project"
 
-	tempDir, err := os.MkdirTemp("", "test-import-with-report")
-	suite.Require().NoError(err)
-
 	defer func() {
-		// delete temp dir
-		os.RemoveAll(tempDir) // nolint: errcheck
-
 		// delete project
 		suite.ExecuteNuctl([]string{"delete", "project", projectName, "--strategy", "cascading"}, nil) // nolint: errcheck
 
 	}()
 
 	// generate report path
-	reportPath := path.Join(tempDir,
+	reportPath := path.Join(suite.tempDir,
 		fmt.Sprintf("import-project-report-%s.json",
 			common.GenerateRandomString(5, common.LettersAndNumbers),
 		),
 	)
-	err = suite.ExecuteNuctl([]string{"import", "project", "--verbose", "--save-report", "--report-file-path", reportPath, functionConfigPath}, nil)
+	err := suite.ExecuteNuctl([]string{"import", "project", "--verbose", "--save-report", "--report-file-path", reportPath, functionConfigPath}, nil)
 	suite.Require().NotNil(err)
 
 	// read a generated report
@@ -1627,7 +1619,6 @@ func (suite *functionExportImportTestSuite) TestImportWithReport() {
 	suite.Require().Equal("There's more than one http trigger (unsupported)", projectReport.FunctionReports.Failed["incorrect-not-fixable-test-function"].FailReason)
 	suite.Require().Equal(false, projectReport.FunctionReports.Failed["incorrect-not-fixable-test-function"].CanBeAutoFixed)
 }
-*/
 
 func (suite *functionExportImportTestSuite) TestExportImportRoundTripFromStdin() {
 	uniqueSuffix := "-" + xid.New().String()
