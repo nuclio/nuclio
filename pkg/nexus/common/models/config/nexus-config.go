9e54b888126edcfab2d39ab0e31ce9dd6493cfd0
konsumgandalf <david_schmidt_privat@outlook.com>
2024-02-01 16:41:05 +0100

    refactor(pkg-nexus): config
    Reflect changes to currentParallelRequests

--- pkg/nexus/common/models/config/nexus-config.go
+++ pkg/nexus/common/models/config/nexus-config.go
@@ -5,13 +5,18 @@ import "sync/atomic"
 // NexusConfig defines the configuration for the nexus. This allows to fine tune the nexus.
 type NexusConfig struct {
 	MaxParallelRequests      *atomic.Int32
+	CurrentParallelRequests  *atomic.Int32
 	FunctionExecutionChannel chan string
 }
 
 // NewNexusConfig allows to create a nexus config.
 func NewNexusConfig(maxParallelRequests *atomic.Int32, executionChannel chan string) NexusConfig {
+	var currentParallelRequests atomic.Int32
+	currentParallelRequests.Store(0)
+
 	return NexusConfig{
 		MaxParallelRequests:      maxParallelRequests,
+		CurrentParallelRequests:  &currentParallelRequests,
 		FunctionExecutionChannel: executionChannel,
 	}
 }
