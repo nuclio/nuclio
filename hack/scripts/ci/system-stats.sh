095482da1edb08eae85cf9c5e4c0af6d2b165fd4
TomerShor <tomer.shor@gmail.com>
2024-02-05 12:28:55 +0200

    system-stats script

---
+++ hack/scripts/ci/system-stats.sh
@@ -0,0 +1,26 @@
+echo "Displaying System Stats"
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+echo "## Memory ##"
+echo "$ free -h"
+free -h
+echo "--------------------------"
+echo "$ ps aux --sort=-%mem | head -n 20"
+ps aux --sort=-%mem | head -n 20
+echo "--------------------------"
+echo "$ top -b -n 1 | head -n 20"
+top -b -n 1 | head -n 20
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+echo "## CPU ##"
+echo "$ lscpu"
+lscpu
+echo "--------------------------"
+echo "$ ps aux | sort -k 3 -r | head -n 20"
+ps aux | sort -k 3 -r | head -n 20
+echo "--------------------------"
+echo "$ mpstat -P ALL"
+mpstat -P ALL
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+echo "## Disk ##"
+echo "$ df --human-readable"
+df --human-readable
+echo "-----------"
