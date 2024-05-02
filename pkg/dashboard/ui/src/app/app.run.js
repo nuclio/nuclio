4e91ff8ef1ad0becfac7b2d9a7728444da0b40a3
Taras.Hlukhovetskyi <taras.hlukhovetskyi@globallogic.com>
2024-05-02 20:04:52 +0300

    Impl UI - Support disabling default http trigger creation for a specific function

--- pkg/dashboard/ui/src/app/app.run.js
+++ pkg/dashboard/ui/src/app/app.run.js
@@ -89,7 +89,7 @@ limitations under the License.
                 backendOptions: [
                     {
                         expirationTime: ConfigService.i18nextExpirationTime,
-                        defaultVersion: 'v0.6'
+                        defaultVersion: 'v0.7'
                     },
                     {
                         loadPath: 'assets/i18n/{{lng}}/{{ns}}.json',
