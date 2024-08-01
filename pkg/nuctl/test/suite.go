7782df0c763a12dd038fe4c732e744267b5b3c28
rokatyy <ekaterina_molchanova@mckinsey.com>
2024-08-01 15:08:17 +0100

    delete project in teardown

--- pkg/nuctl/test/suite.go
+++ pkg/nuctl/test/suite.go
@@ -128,6 +128,8 @@ func (suite *Suite) TearDownSuite() {
 
 	err = os.RemoveAll(suite.tempDir)
 	suite.Require().NoError(err, "Failed to remove temp dir - %s", suite.tempDir)
+	// remove project
+	suite.ExecuteNuctl([]string{"delete", "project", platform.DefaultProjectName}, map[string]string{}) // nolint: errcheck
 
 	suite.logger.Debug("Suite tear down completed")
 }
