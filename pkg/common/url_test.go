12a11552aab942b8aa7d2bf37ecf5f9ea9ed19c5
Katerina Molchanova <35141662+rokatyy@users.noreply.github.com>
2024-08-09 11:55:20 +0300

    [Dashboard] Add path validation for api gateway [1.13.x] (#3315)
    [Dashboard] Add path validation for api gateway

--- pkg/common/url_test.go
+++ pkg/common/url_test.go
@@ -81,6 +81,26 @@ func (ts *IsURLTestSuite) TestNormalizeURLPath() {
 	}
 }
 
+func (ts *IsURLTestSuite) TestValidatePath() {
+	for inputPath, expectedOutput := range map[string]bool{
+		"/path/to/resource":           true,
+		"another/path/to/resource123": true,
+		"/yet-another_path":           true,
+		"yet-another_path":            true,
+		"invalid path":                false,
+		"/":                           true,
+		"":                            true,
+		"simplepath":                  true,
+		"complex/path-with-123_and-mixed_characters/": true,
+		"/path/with/special/characters/!@#$%^&*()":    false,
+		"/path/with/encoded/characters/%20":           false,
+		"another/weird@path":                          false,
+		"/path/with?query=parameters&and=more":        false,
+	} {
+		ts.Require().Equal(ValidateURLPath(inputPath), expectedOutput)
+	}
+}
+
 func (ts *DownloadFileTestSuite) TestDownloadFile() {
 	content := "content"
 	errResult := ts.testDownloadFile(func(req *http.Request) (*http.Response, error) {
