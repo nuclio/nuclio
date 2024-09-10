7038eabfbbae687d7720784ab7d24cf495e80310
rokatyy <ekaterina_molchanova@mckinsey.com>
2024-09-10 09:32:42 +0100

    Do not enrich workerAllocatorName by default

--- pkg/platform/abstract/platform.go
+++ pkg/platform/abstract/platform.go
@@ -1797,15 +1797,6 @@ func (ap *Platform) enrichTriggers(ctx context.Context, functionConfig *function
 			}
 		}
 
-		if triggerInstance.WorkerAllocatorName == "" {
-			triggerInstance.WorkerAllocatorName = triggerName
-			ap.Logger.DebugWithCtx(ctx, "Enriched WorkerAllocatorName",
-				"triggerName", triggerName,
-				"triggerKind", triggerInstance.Kind,
-				"WorkerAllocatorName", triggerInstance.WorkerAllocatorName,
-			)
-		}
-
 		functionConfig.Spec.Triggers[triggerName] = triggerInstance
 	}
 
