7e38b4e0125c16a67509581b8841982e8ba7fdd8
TomerShor <90552140+TomerShor@users.noreply.github.com>
2025-01-16 10:50:24 +0200

    [Processor] Stop triggers before termination (#3452)
    When getting a SIGTERM from k8s, stop all triggers so they won't handle
    new incoming events.
    
    In HTTP trigger, use `SafeStatus` to avoid concurrent RW on the
    trigger's status.
    
    https://iguazio.atlassian.net/browse/NUC-298

--- pkg/platform/kube/test/platform_test.go
+++ pkg/platform/kube/test/platform_test.go
@@ -468,7 +468,7 @@ func (suite *DeployFunctionTestSuite) TestSecurityContext() {
 			runAsGroupID,
 			fsGroup),
 			strings.TrimSpace(results.Output))
-		suite.InvokeFunction("GET", deployResult.Port, "", nil)
+		suite.InvokeFunction("GET", deployResult.Port, "", nil, true)
 		return true
 	})
 }
@@ -1846,6 +1846,7 @@ func (suite *DeployFunctionTestSuite) TestProcessorShutdown() {
 	// change source code
 	createFunctionOptions.FunctionConfig.Spec.Build.FunctionSourceCode = base64.StdEncoding.EncodeToString([]byte(`
 terminated = False
+
 def handler(context, event):
     if terminated:
         context.logger.info("Got event after termination")
@@ -1877,7 +1878,7 @@ def init_context(context):
 			for {
 				select {
 				case <-ticker.C:
-					suite.InvokeFunction("GET", deployResult.Port, "", nil)
+					suite.InvokeFunction("GET", deployResult.Port, "", nil, false)
 				case <-ctx.Done():
 					return
 				}
@@ -1890,12 +1891,13 @@ def init_context(context):
 		err = suite.KubeClientSet.CoreV1().Pods(firstPod.Namespace).Delete(suite.Ctx, firstPod.Name, metav1.DeleteOptions{})
 		suite.Require().NoError(err)
 
-		err = suite.WaitMessageInPodLog(firstPod.Namespace, firstPod.Name, "All triggers are terminated", &v1.PodLogOptions{}, 30*time.Second)
-		suite.Require().NoError(err, "triggers were not terminated")
 		cancel()
 
-		err = suite.WaitMessageInPodLog(firstPod.Namespace, firstPod.Name, "Got event after termination", &v1.PodLogOptions{}, 20*time.Second)
-		suite.Require().NotNil(err)
+		err = suite.WaitMessageInPodLog(firstPod.Namespace, firstPod.Name, "Stopping HTTP trigger", &v1.PodLogOptions{}, 20*time.Second)
+		suite.Require().NoError(err)
+
+		err = suite.WaitMessageInPodLog(firstPod.Namespace, firstPod.Name, "All triggers are terminated", &v1.PodLogOptions{}, 30*time.Second)
+		suite.Require().NoError(err, "triggers were not terminated")
 		return true
 	})
 }
