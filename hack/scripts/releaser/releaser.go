bb6c2dd5682aed544c77f5f8a71a5bf4c34185c7
rokatyy <ekaterina_molchanova@mckinsey.com>
2025-02-25 15:18:19 +0000

    linux sed format

--- hack/scripts/releaser/releaser.go
+++ hack/scripts/releaser/releaser.go
@@ -276,14 +276,14 @@ func (r *Release) bumpHelmChartVersion() error {
 
 	// explicitly bump the app version
 	if _, err := r.cmdRunner.Run(runOptions,
-		`sed -i '' -e "s/^\(appVersion: \).*$/\1%s/g" %s`,
+		`sed -i -e "s/^\(appVersion: \).*$/\1%s/g" %s`,
 		r.targetVersion,
 		r.resolveHelmChartFullPath()); err != nil {
 		return errors.Wrap(err, "Failed to write helm chart target version")
 	}
 
 	if _, err := r.cmdRunner.Run(runOptions,
-		`sed -i '' -e "s/^\(version: \).*$/\1%s/g" %s`,
+		`sed -i  -e "s/^\(version: \).*$/\1%s/g" %s`,
 		r.helmChartsTargetVersion,
 		r.resolveHelmChartFullPath()); err != nil {
 		return errors.Wrap(err, "Failed to write helm chart target version")
