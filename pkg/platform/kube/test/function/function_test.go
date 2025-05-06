1a063a6282924c8de74b42c74f4c3f7e82283cd7
Noam Weiler <noamweiler1@gmail.com>
2025-05-06 19:24:59 +0300

    [Controller] Enable Readiness and Liveness probes configurations (#3629)
    ### **Motivation**  
    Described in the [JIRA](https://iguazio.atlassian.net/browse/NUC-409)
    
    ### **Root Cause**
    Nuclio functions previously used default values for readiness and
    liveness probes, with no user-facing way to customize them. This limited
    flexibility in production environments, where probe tuning is often
    necessary to accommodate different workloads.
    
    ### **Description**  
    This PR introduces support for configuring readiness and liveness probes
    by propagating the probe configurations to the container-level
    configuration.
    
    ### **Affected Areas**  
    - [Refactor unit
    tests](https://github.com/nuclio/nuclio/pull/3629/commits/99f186ac07d6ea97db4b6ad2591fc133742c0796)
    - Added the defaults `functionInstance.Spec.LivenessProbe` and
    `functionInstance.Spec.ReadinessProbe` to other unit tests.
    This addition was necessary because some tests invoke
    [lazyClient.CreateOrUpdate](https://github.com/nuclio/nuclio/blob/fa3161f95044d9703514802d0fe32509dfa91e0f/pkg/platform/kube/functionres/lazy.go#L165)
    which is affected by this PR.
    These changes maintain the tests' consistency.
    
    ### **Testing**  
    - `CreateOrUpdate` changes were Covered by unit tests
    - The full probes propagation flow is covered with an [Integration
    test](https://github.com/nuclio/nuclio/pull/3629/commits/d309668737646ccd5aed2ea13b6ec2556124aa26#diff-c624dd2cc2b57bbbf2861daae4140a94323f2d99405f4ed3de1cf6c9fff76480R1336)
    
    ### **Changes Made**  
    - Modified the `ReadinessProbe` and `LivenessProbe` fields to be taken
    from the function specification instead of using static values.

--- pkg/platform/kube/test/function/function_test.go
+++ pkg/platform/kube/test/function/function_test.go
@@ -1333,6 +1333,67 @@ func (suite *DeployFunctionTestSuite) TestDeployImportedFunctionAsScaledToZero()
 	suite.Require().Equal(int(*deployment.Spec.Replicas), 0)
 }
 
+func (suite *DeployFunctionTestSuite) TestCreateFunctionWithProbes() {
+	// This test validates that the liveness and readiness probes are set correctly
+	// when creating a function with custom probes.
+	// The test will create a function with custom liveness.TimeoutSeconds probe.
+	// All the other liveness and readiness probes should be taken from the platform default config.
+
+	// Prepare
+	functionName := "func-hello-world"
+	livenessProbeTimeoutSeconds := int32(14)
+	createFunctionOptions := suite.CompileCreateFunctionOptions(functionName)
+	createFunctionOptions.FunctionConfig.Spec.LivenessProbe = &v1.Probe{TimeoutSeconds: livenessProbeTimeoutSeconds}
+	getFunctionOptions := &platform.GetFunctionsOptions{
+		Name:      createFunctionOptions.FunctionConfig.Meta.Name,
+		Namespace: createFunctionOptions.FunctionConfig.Meta.Namespace,
+	}
+
+	suite.DeployFunction(createFunctionOptions, func(deployResults *platform.CreateFunctionResult) bool {
+
+		// get the function
+		function := suite.GetFunction(getFunctionOptions)
+
+		// ensure function is ready
+		suite.Require().Equal(functionconfig.FunctionStateReady, function.GetStatus().State)
+
+		// ensure function pods are running
+		suite.WaitForFunctionPods(functionName, time.Minute, func(pods []v1.Pod) bool {
+			suite.Logger.DebugWith("Ensure function pods are running", "pods", pods)
+			for _, pod := range pods {
+				if pod.Status.Phase != v1.PodRunning {
+					return false
+				}
+			}
+			return true
+		})
+
+		// get the function's deployment and validate it has the probes
+		podsList := suite.GetFunctionPods(functionName)
+		suite.Require().Len(podsList, 1)
+		suite.Require().Len(podsList[0].Spec.Containers, 1)
+		container := podsList[0].Spec.Containers[0]
+		suite.Require().NotNil(container)
+		suite.Require().NotNil(container.ReadinessProbe)
+		suite.Require().NotNil(container.LivenessProbe)
+
+		// Assert liveness probe - should take all values from the platform default config except timeout
+		liveProbe := container.LivenessProbe
+		suite.Require().Equal(liveProbe.TimeoutSeconds, livenessProbeTimeoutSeconds)
+		suite.Require().Equal(liveProbe.PeriodSeconds, platformconfig.DefaultLivenessProbeConfiguration.PeriodSeconds)
+		suite.Require().Equal(liveProbe.FailureThreshold, platformconfig.DefaultLivenessProbeConfiguration.FailureThreshold)
+		suite.Require().Equal(liveProbe.InitialDelaySeconds, platformconfig.DefaultLivenessProbeConfiguration.InitialDelaySeconds)
+		// Assert readiness probe - should take all values from the platform default config
+		readinessProbe := container.ReadinessProbe
+		suite.Require().Equal(readinessProbe.InitialDelaySeconds, platformconfig.DefaultReadinessProbeConfiguration.InitialDelaySeconds)
+		suite.Require().Equal(readinessProbe.TimeoutSeconds, platformconfig.DefaultReadinessProbeConfiguration.TimeoutSeconds)
+		suite.Require().Equal(readinessProbe.PeriodSeconds, platformconfig.DefaultReadinessProbeConfiguration.PeriodSeconds)
+		suite.Require().Equal(readinessProbe.FailureThreshold, platformconfig.DefaultReadinessProbeConfiguration.FailureThreshold)
+
+		return true
+	})
+}
+
 func (suite *DeployFunctionTestSuite) TestCreateFunctionWithCustomScalingMetrics() {
 	one := 1
 	four := 4
