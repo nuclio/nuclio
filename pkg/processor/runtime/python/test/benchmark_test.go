1b6361c492c5d2bde105e43fb67be74c220665dd
rokatyy <ekaterina_molchanova@mckinsey.com>
2025-06-30 10:21:16 +0100

    benchmark

--- pkg/processor/runtime/python/test/benchmark_test.go
+++ pkg/processor/runtime/python/test/benchmark_test.go
@@ -1,18 +1,36 @@
 //go:build test_integration && test_local && test_benchmark
 
+/*
+Copyright 2025 The Nuclio Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package test
 
 import (
 	"fmt"
-	"github.com/nuclio/nuclio/pkg/functionconfig"
-	"github.com/nuclio/nuclio/pkg/platform"
-	httpsuite "github.com/nuclio/nuclio/pkg/processor/trigger/http/test/suite"
-	"github.com/stretchr/testify/suite"
 	"net/http"
 	"os"
 	"path"
 	"testing"
 	"time"
+
+	"github.com/nuclio/nuclio/pkg/functionconfig"
+	"github.com/nuclio/nuclio/pkg/platform"
+	httpsuite "github.com/nuclio/nuclio/pkg/processor/trigger/http/test/suite"
+
+	"github.com/stretchr/testify/suite"
 )
 
 type BenchmarkTestSuite struct {
@@ -29,7 +47,13 @@ func (suite *BenchmarkTestSuite) SetupTest() {
 	suite.FunctionDir = path.Join(suite.GetNuclioSourceDir(), "pkg", "processor", "runtime", "python", "test")
 
 	var err error
-	suite.reportFile, err = os.OpenFile("benchmark_report.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+
+	reportPath := os.Getenv("BENCHMARK_REPORT_PATH")
+	if reportPath == "" {
+		reportPath = "benchmark_report.txt" // default fallback
+	}
+
+	suite.reportFile, err = os.OpenFile(reportPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 	suite.Require().NoError(err)
 }
 
