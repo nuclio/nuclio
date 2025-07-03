eab93b7ca8c956516e3003af8ae1aa0e4a9e76f1
rokatyy <ekaterina_molchanova@mckinsey.com>
2025-07-03 09:26:40 +0100

    Moran CR

--- pkg/auth/iguazio/v4/auth.go
+++ pkg/auth/iguazio/v4/auth.go
@@ -21,26 +21,25 @@ import (
 
 	authpkg "github.com/nuclio/nuclio/pkg/auth"
 	"github.com/nuclio/nuclio/pkg/auth/iguazio"
-	"github.com/nuclio/nuclio/pkg/auth/iguazio/v1"
 
 	"github.com/nuclio/logger"
 	"github.com/nuclio/nuclio-sdk-go"
 )
 
-type AuthV4 struct {
+type Auth struct {
 	*iguazio.AbstractAuth
 }
 
 func NewAuth(logger logger.Logger, config *authpkg.Config) authpkg.Auth {
-	return &v1.Auth{
+	return &Auth{
 		AbstractAuth: iguazio.NewAbstractAuth(logger, config),
 	}
 }
 
-func (a *AuthV4) Authenticate(request *http.Request, options *authpkg.Options) (authpkg.Session, error) {
+func (a *Auth) Authenticate(request *http.Request, options *authpkg.Options) (authpkg.Session, error) {
 	return nil, nuclio.ErrNotImplemented
 }
 
-func (a *AuthV4) Middleware(options *authpkg.Options) func(next http.Handler) http.Handler {
+func (a *Auth) Middleware(options *authpkg.Options) func(next http.Handler) http.Handler {
 	return a.AbstractAuth.Middleware(a.Authenticate, options)
 }
