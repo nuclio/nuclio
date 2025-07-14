5ed94a4a9b5246b13a6fb8e0fc294f02c97e7a33
rokatyy <ekaterina_molchanova@mckinsey.com>
2025-07-14 14:50:55 +0100

    [Images] Take images from gcr instead of dockerhub

--- pkg/processor/build/runtime/nodejs/runtime.go
+++ pkg/processor/build/runtime/nodejs/runtime.go
@@ -39,7 +39,7 @@ func (n *nodejs) GetProcessorDockerfileInfo(runtimeConfig *runtimeconfig.Config,
 	processorDockerfileInfo := runtime.ProcessorDockerfileInfo{}
 
 	// set the default base image
-	processorDockerfileInfo.BaseImage = "node:10.20-alpine"
+	processorDockerfileInfo.BaseImage = "gcr.io/iguazio/node:10.20-alpine"
 
 	processorDockerfileInfo.ImageArtifactPaths = map[string]string{
 		"handler": "/opt/nuclio",
