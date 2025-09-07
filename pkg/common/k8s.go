579dea76bf1bd5d1e71867a8a2e47f012f55a0b4
weilerN <noamweiler1@gmail.com>
2025-09-07 18:37:52 +0300

    Move nuclio self namespace to const

--- pkg/common/k8s.go
+++ pkg/common/k8s.go
@@ -31,6 +31,10 @@ import (
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 )
 
+const (
+	NuclioSelfNamespace = "@nuclio.selfNamespace"
+)
+
 func IsInKubernetesCluster() bool {
 	return len(os.Getenv("KUBERNETES_SERVICE_HOST")) != 0 && len(os.Getenv("KUBERNETES_SERVICE_PORT")) != 0
 }
@@ -73,7 +77,7 @@ func ResolveNamespace(namespaceArgument string, defaultEnvVarKey string) string 
 	}
 
 	// if the namespace exists in env, use that, else, assume "this" namespace
-	return ResolveDefaultNamespace(GetEnvOrDefaultString(defaultEnvVarKey, "@nuclio.selfNamespace"))
+	return ResolveDefaultNamespace(GetEnvOrDefaultString(defaultEnvVarKey, NuclioSelfNamespace))
 }
 
 // ResolveDefaultNamespace returns the proper default resource namespace, given the current default namespace
@@ -81,7 +85,7 @@ func ResolveDefaultNamespace(namespace string) string {
 
 	defaultNamespace := "default"
 	switch namespace {
-	case "@nuclio.selfNamespace":
+	case NuclioSelfNamespace:
 
 		// for k8s
 		if IsInKubernetesCluster() {
