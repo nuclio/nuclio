b57cdd879c8b68ff376a1e21dba0cc64501a7f90
plparent <45149203+plparent@users.noreply.github.com>
2025-11-27 08:03:35 -0500

    [Trigger] Nats Jetstream Options (#3895)
    ### 📝 Description
    I've been using nuclio with the nats jetstream trigger. However,
    sometimes, my nats instance goes down for a few minutes. By default the
    nats client will attempt to reconnect for 2 minutes which often is not
    enough for my use case. This means I end up with dead nuclio functions
    that must be restarted. My solution is to add a way to configure the
    nats client through the nuclio trigger configuration.
    
    ---
    
    ### 🛠️ Changes Made
    Add Nats client options in the types.go for the nats jetstream trigger.
    
    ---
    
    ### ✅ Checklist
    - [x] I updated the documentation (if applicable)
    - [x] I have tested the changes in this PR
    
    ---
    
    ### 🧪 Testing
    I performed manual testing to make sure that the options are passed
    successfully to the nats client.
    I also intentionally crashed my nats instance to see if nuclio functions
    would reconnect after an extend amount of time.
    
    ---
    
    ### 🔗 References
    - Ticket link:
    - Design docs links:
    - External links:
    https://github.com/nats-io/nats.go/blob/main/nats.go#L285
    
    ---
    
    ### 🚨 Breaking Changes?
    
    - [ ] Yes (explain below)
    - [x] No
    ---
    
    ### 🔍️ Additional Notes

--- pkg/processor/trigger/nats/jetstream/types.go
+++ pkg/processor/trigger/nats/jetstream/types.go
@@ -17,21 +17,45 @@ limitations under the License.
 package natsjetstream
 
 import (
+	"time"
+
 	"github.com/nuclio/nuclio/pkg/functionconfig"
 	"github.com/nuclio/nuclio/pkg/processor/runtime"
 	"github.com/nuclio/nuclio/pkg/processor/trigger"
 
 	"github.com/mitchellh/mapstructure"
+	nats "github.com/nats-io/nats.go"
 	"github.com/nuclio/errors"
+	"github.com/nuclio/logger"
 )
 
 type Configuration struct {
 	trigger.Configuration
 	Stream   string
 	Consumer string
+
+	// NATS connection configuration
+	AllowReconnect  bool
+	MaxReconnect    int
+	ReconnectWait   string
+	ReconnectJitter string
+	Timeout         string
+	DrainTimeout    string
+	FlusherTimeout  string
+	PingInterval    string
+	MaxPingsOut     int
+
+	// resolved fields
+	reconnectWait   time.Duration
+	reconnectJitter time.Duration
+	timeout         time.Duration
+	drainTimeout    time.Duration
+	flusherTimeout  time.Duration
+	pingInterval    time.Duration
 }
 
-func NewConfiguration(id string,
+func NewConfiguration(logger logger.Logger,
+	id string,
 	triggerConfiguration *functionconfig.Trigger,
 	runtimeConfiguration *runtime.Configuration) (*Configuration, error) {
 	newConfiguration := Configuration{}
@@ -48,7 +72,99 @@ func NewConfiguration(id string,
 		return nil, errors.Wrap(err, "Failed to decode attributes")
 	}
 
-	// TODO: validate
+	err = newConfiguration.PopulateConfigurationFromAnnotations([]trigger.AnnotationConfigField{
+		{Key: "nuclio.io/nats-jetstream-allow-reconnect", ValueBool: &newConfiguration.AllowReconnect},
+		{Key: "nuclio.io/nats-jetstream-max-reconnect", ValueInt: &newConfiguration.MaxReconnect},
+		{Key: "nuclio.io/nats-jetstream-reconnect-wait", ValueString: &newConfiguration.ReconnectWait},
+		{Key: "nuclio.io/nats-jetstream-reconnect-jitter", ValueString: &newConfiguration.ReconnectJitter},
+		{Key: "nuclio.io/nats-jetstream-timeout", ValueString: &newConfiguration.Timeout},
+		{Key: "nuclio.io/nats-jetstream-drain-timeout", ValueString: &newConfiguration.DrainTimeout},
+		{Key: "nuclio.io/nats-jetstream-flusher-timeout", ValueString: &newConfiguration.FlusherTimeout},
+		{Key: "nuclio.io/nats-jetstream-ping-interval", ValueString: &newConfiguration.PingInterval},
+		{Key: "nuclio.io/nats-jetstream-max-pings-out", ValueInt: &newConfiguration.MaxPingsOut},
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to populate configuration from annotations")
+	}
+
+	// set default values and parse durations
+	if newConfiguration.MaxReconnect == 0 {
+		newConfiguration.MaxReconnect = 60
+	}
+
+	if newConfiguration.MaxPingsOut == 0 {
+		newConfiguration.MaxPingsOut = 2
+	}
+
+	for _, durationConfigField := range []trigger.DurationConfigField{
+		{
+			Name:    "reconnect wait",
+			Value:   newConfiguration.ReconnectWait,
+			Field:   &newConfiguration.reconnectWait,
+			Default: 2 * time.Second,
+		},
+		{
+			Name:    "reconnect jitter",
+			Value:   newConfiguration.ReconnectJitter,
+			Field:   &newConfiguration.reconnectJitter,
+			Default: 100 * time.Millisecond,
+		},
+		{
+			Name:    "timeout",
+			Value:   newConfiguration.Timeout,
+			Field:   &newConfiguration.timeout,
+			Default: 2 * time.Second,
+		},
+		{
+			Name:    "drain timeout",
+			Value:   newConfiguration.DrainTimeout,
+			Field:   &newConfiguration.drainTimeout,
+			Default: 30 * time.Second,
+		},
+		{
+			Name:    "flusher timeout",
+			Value:   newConfiguration.FlusherTimeout,
+			Field:   &newConfiguration.flusherTimeout,
+			Default: 1 * time.Minute,
+		},
+		{
+			Name:    "ping interval",
+			Value:   newConfiguration.PingInterval,
+			Field:   &newConfiguration.pingInterval,
+			Default: 2 * time.Minute,
+		},
+	} {
+		if err = newConfiguration.ParseDurationOrDefault(&durationConfigField); err != nil {
+			return nil, err
+		}
+	}
+
+	if newConfiguration.Stream == "" {
+		return nil, errors.New("Stream must be set")
+	}
+
+	if newConfiguration.Consumer == "" {
+		return nil, errors.New("Consumer must be set")
+	}
 
 	return &newConfiguration, nil
+}
+
+func (c *Configuration) GetNATSOptions() []nats.Option {
+	var opts []nats.Option
+
+	if !c.AllowReconnect {
+		opts = append(opts, nats.NoReconnect())
+	}
+
+	opts = append(opts, nats.MaxReconnects(c.MaxReconnect))
+	opts = append(opts, nats.ReconnectWait(c.reconnectWait))
+	opts = append(opts, nats.ReconnectJitter(c.reconnectJitter, c.reconnectJitter*10)) // second argument is used if TLS is enabled
+	opts = append(opts, nats.Timeout(c.timeout))
+	opts = append(opts, nats.DrainTimeout(c.drainTimeout))
+	opts = append(opts, nats.FlusherTimeout(c.flusherTimeout))
+	opts = append(opts, nats.PingInterval(c.pingInterval))
+	opts = append(opts, nats.MaxPingsOutstanding(c.MaxPingsOut))
+
+	return opts
 }
