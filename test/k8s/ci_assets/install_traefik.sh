13c28017a989a0501ea18b30f234d51799ecd305
Emilio Cafe' Nunes <emilio_cafe_nunes@external.mckinsey.com>
2026-04-16 11:02:37 +0200

    [CI] Replace ingress-nginx with Traefik (#4086)
    ### 📝 Description
    <!-- A short summary of what this PR does. -->
    <!-- Include any relevant context or background information. -->
    Install Traefik instead of ingress-nginx for running CI tests
    
    ---
    
    ### 🛠️ Changes Made
    <!-- - Key changes (e.g., added feature X, refactored Y, fixed Z) -->
    - Deleted install_nginx_ingress_controller.sh
    - Added install_traefik.sh
    - Changed CI actions to use Traefik installation script
    - Upgraded Helm version to latest v3 available, required for Traefik
    chart
    
    ---
    
    ### ✅ Checklist
    - [ ] I updated the documentation (if applicable)
    - [x] I have tested the changes in this PR
    
    ---
    
    ### 🧪 Testing
    <!-- - How it was tested (unit tests, manual, integration) -->  
    <!-- - Any special cases covered. -->  
    - Integration Tests
    
    ---
    
    ### 🔗 References
    - Ticket link: https://iguazio.atlassian.net/browse/IG4-1927
    - Design docs links:
    - External links:
    
    ---
    
    ### 🚨 Breaking Changes?
    
    - [ ] Yes (explain below)
    - [x] No
    
    <!-- If yes, describe what needs to be changed downstream: -->
    
    ---
    
    ### 🔍️ Additional Notes
    <!-- Anything else reviewers should know (follow-up tasks, known issues,
    affected areas etc.). -->
    <!-- ### 📸 Screenshots / Logs -->

---
+++ test/k8s/ci_assets/install_traefik.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# Copyright 2026 The Nuclio Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# install Gateway API CRDs
+kubectl apply --server-side -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.5.1/standard-install.yaml
+
+# install traefik
+helm repo add traefik https://traefik.github.io/charts
+helm repo update
+
+helm install traefik traefik/traefik \
+  --namespace traefik --create-namespace \
+  --set ports.web.hostPort=80 \
+  --set ports.websecure.hostPort=443 \
+  --set deployment.kind=DaemonSet \
+  --set service.type=ClusterIP \
+  --set providers.kubernetesIngressNGINX.enabled=true \
+  --set providers.kubernetesIngressNGINX.publishStatusAddress="ingress.ready.local" \
+  --set providers.kubernetesGateway.enabled=true \
+  --set gateway.enabled=false \
+  --set gatewayClass.enabled=true \
+  --version 40.0.0-ea.3
+
+# create nginx IngressClass standalone resource mapping to Traefik
+kubectl apply -f - <<EOF
+apiVersion: networking.k8s.io/v1
+kind: IngressClass
+metadata:
+  name: nginx
+spec:
+  controller: k8s.io/ingress-nginx
+EOF
